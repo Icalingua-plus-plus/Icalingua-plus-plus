a80b38c75113a1c7cccd8781bdfcefafa269f1fe
clansty <i@gao4.pw>
2021-01-25 16:59:03 +0800

    init

---
+++ src/renderer/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import axios from 'axios'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './App'
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.http = Vue.prototype.$http = axios
+Vue.config.productionTip = false
+
+Vue.use(ElementUI);
+
+/* eslint-disable no-new */
+new Vue({
+  components: { App },
+  template: '<App/>'
+}).$mount('#app')
