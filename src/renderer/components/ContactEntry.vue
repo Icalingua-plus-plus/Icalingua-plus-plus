3ce16473476519f7f5918f3ff5a470c800007bfd
clansty <i@gao4.pw>
2021-02-04 13:33:02 +0800

    contacts panel

---
+++ src/renderer/components/ContactEntry.vue
@@ -0,0 +1,105 @@
+<template>
+	<a @click="$emit('click')" @click.right="$emit('contextmenu')">
+		<div class="root">
+			<div class="entry">
+				<div class="left">
+					<el-avatar
+						size="large"
+						:src="
+							id < 0
+								? `https://p.qlogo.cn/gh/${-id}/${-id}/0`
+								: `http://q1.qlogo.cn/g?b=qq&nk=${id}&s=640`
+						"
+					/>
+				</div>
+				<div class="right">
+					<div class="flex l1">
+						<div class="name">
+							{{ remark }}
+							<span class="rawname" v-show="name && name != remark">
+								({{ name }})
+							</span>
+						</div>
+					</div>
+					<div class="flex">
+						<div class="desc">
+							{{ displayId }}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</a>
+</template>
+
+<script>
+	export default {
+		name: "ContactEntry",
+		props: ['id', 'name', 'remark'],
+		computed:{
+			displayId(){
+				return Math.abs(this.id)
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.root {
+		padding: 0 15px;
+		transition: background-color 0.3s;
+		cursor: default;
+	}
+	.root:hover {
+		background-color: #f2f6fc;
+	}
+	div.entry {
+		padding: 10px 0;
+		height: 50px;
+		display: flex;
+		align-items: center;
+		border-bottom: 1px solid #e4e7ed;
+	}
+	.left {
+		width: max-content;
+		float: left;
+		padding-top: 5px;
+	}
+	.right {
+		margin-left: 15px;
+		width: 100%;
+	}
+	a {
+		text-decoration: none;
+	}
+	.desc {
+		color: #606266;
+		font-size: 12px;
+		text-overflow: ellipsis;
+		overflow: hidden;
+		white-space: nowrap;
+		width: 0;
+		flex: 1;
+	}
+	.name {
+		font-weight: bold;
+		color: #303133;
+		text-overflow: ellipsis;
+		overflow: hidden;
+		white-space: nowrap;
+		width: 0;
+		flex: 1;
+		font-size: 16px;
+	}
+	.flex {
+		display: flex;
+		height: 18px;
+	}
+	.l1 {
+		height: 25px;
+	}
+	.rawname {
+		color: #909399;
+		font-size: 14px;
+	}
+</style>
---
+++ src/renderer/components/TheContactsPanel.vue
@@ -0,0 +1,54 @@
+<template>
+	<div class="root">
+		<el-tabs v-model="activeName" :stretch="true">
+			<el-tab-pane label="Friends" name="friends"></el-tab-pane>
+			<el-tab-pane label="Groups" name="groups"></el-tab-pane>
+		</el-tabs>
+		<div v-show="activeName == 'friends'">
+			<ContactEntry
+				v-for="i in friends"
+				:key="i.user_id"
+				:id="i.user_id"
+				:remark="i.remark"
+				:name="i.nickname"
+			/>
+		</div>
+		<div v-show="activeName == 'groups'">
+			<ContactEntry
+				v-for="i in groups"
+				:key="i.group_id"
+				:id="-i.group_id"
+				:remark="i.group_name"
+			/>
+		</div>
+	</div>
+</template>
+
+<script>
+	import { remote } from 'electron'
+	import ContactEntry from './ContactEntry.vue'
+
+	const bot = remote.getGlobal("bot")
+
+	export default {
+		components: { ContactEntry },
+		data() {
+			return {
+				activeName: 'friends',
+				groups: [],
+				friends: [],
+			}
+		},
+		created() {
+			bot.getFriendList().data.forEach(e => this.friends.push(e))
+			bot.getGroupList().data.forEach(e => this.groups.push(e))
+		}
+	}
+</script>
+
+<style scoped>
+	.root {
+		overflow-y: auto;
+        height: 100%;
+	}
+</style>
