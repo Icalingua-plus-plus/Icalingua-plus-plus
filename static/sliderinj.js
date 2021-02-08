f43319d037eb47a41670c4c0a16b01f76df93636
clansty <i@gao4.pw>
2021-02-08 20:53:33 +0800

    fix

---
+++ static/sliderinj.js
@@ -0,0 +1,68 @@
+!(() => {
+    let prompt = window.prompt;
+
+    // jsbridge://CAPTCHA/onVerifyCAPTCHA?p=....#2
+    /**
+     * @type {string} url
+     * @return {boolean}
+     */
+    function processUrl(url) {
+        let prefix = "jsbridge://CAPTCHA/onVerifyCAPTCHA?p="
+        if (url.startsWith(prefix)) {
+            let json = url.substring(prefix.length);
+            for (let i = json.length; i--; i > 0) {
+                let j = json.substr(0, i)
+                console.log(j);
+                try {
+                    let content = decodeURIComponent(j);
+                    let obj = JSON.parse(content);
+                    console.log(obj);
+                    window.miraiSeleniumComplete = content;
+                    const remote=require('electron').remote
+                    const bot=remote.getGlobal('bot')
+                    bot.sliderLogin(obj.ticket);
+                    remote.getCurrentWindow().destroy()
+                    break;
+                } catch (ignore) {
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    (() => {
+        let desc = Object.getOwnPropertyDescriptor(Image.prototype, "src");
+        Object.defineProperty(Image.prototype, "src", {
+            get: desc.get,
+            set(v) {
+                if (processUrl(v)) return;
+                desc.set.call(this, v)
+            }
+        })
+    })();
+
+
+    (() => {
+        let desc = Object.getOwnPropertyDescriptor(HTMLIFrameElement.prototype, "src");
+        Object.defineProperty(HTMLIFrameElement.prototype, "src", {
+            get: desc.get,
+            set(v) {
+                if (processUrl(v)) return;
+                desc.set.call(this, v)
+            }
+        })
+    })();
+
+    (() => {
+        let UserAgent = "${MIRAI_SELENIUM-USERAGENT}";
+        if (UserAgent !== "${MIRAI_SELENIUM-USERAGENT}") {
+            Object.defineProperty(Navigator.prototype, "userAgent", {
+                get() {
+                    return UserAgent
+                }
+            });
+            document.querySelectorAll("script").forEach(it => it.remove());
+        }
+    })();
+})()
