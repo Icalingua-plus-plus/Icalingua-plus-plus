f4bedce763a1964da78007149f0ee3c6414c146b
Clansty <i@gao4.pw>
2021-02-22 14:11:33 +0800

    right click copy id

--- src/renderer/components/ContactEntry.vue
+++ src/renderer/components/ContactEntry.vue
@@ -1,5 +1,5 @@
 <template>
-	<a @dblclick="$emit('dblclick')" @click.right="$emit('contextmenu')">
+	<div @dblclick="$emit('dblclick')" @click.right="ctx">
 		<div class="root">
 			<div class="entry">
 				<div class="left">
@@ -16,7 +16,10 @@
 					<div class="flex l1">
 						<div class="name">
 							{{ remark }}
-							<span class="rawname" v-show="name && name != remark">
+							<span
+								class="rawname"
+								v-show="name && name != remark"
+							>
 								({{ name }})
 							</span>
 						</div>
@@ -29,77 +32,108 @@
 				</div>
 			</div>
 		</div>
-	</a>
+	</div>
 </template>
 
 <script>
-	export default {
-		name: "ContactEntry",
-		props: ['id', 'name', 'remark'],
-		computed:{
-			displayId(){
-				return Math.abs(this.id)
+import { remote, clipboard } from 'electron'
+export default {
+	name: "ContactEntry",
+	props: ['id', 'name', 'remark'],
+	computed: {
+		displayId() {
+			return Math.abs(this.id)
+		}
+	},
+	methods: {
+		ctx() {
+			const menu = new remote.Menu()
+			if (this.remark) {
+				menu.append(new remote.MenuItem({
+					label: `Copy "${this.remark}"`,
+					click: () => {
+						clipboard.writeText(this.remark)
+					}
+				}))
 			}
+			if (this.name) {
+				menu.append(new remote.MenuItem({
+					label: `Copy "${this.name}"`,
+					click: () => {
+						clipboard.writeText(this.name)
+					}
+				}))
+			}
+			if (this.displayId) {
+				menu.append(new remote.MenuItem({
+					label: `Copy "${this.displayId}"`,
+					click: () => {
+						clipboard.writeText(this.displayId.toString())
+					}
+				}))
+			}
+			menu.popup({ window: remote.getCurrentWindow() });
 		}
 	}
+}
 </script>
 
 <style scoped>
-	.root {
-		padding: 0 15px;
-		transition: background-color 0.3s;
-		cursor: default;
-	}
-	.root:hover {
-		background-color: #f2f6fc;
-	}
-	div.entry {
-		padding: 10px 0;
-		height: 50px;
-		display: flex;
-		align-items: center;
-		border-bottom: 1px solid #e4e7ed;
-	}
-	.left {
-		width: max-content;
-		float: left;
-		padding-top: 5px;
-	}
-	.right {
-		margin-left: 15px;
-		width: 100%;
-	}
-	a {
-		text-decoration: none;
-	}
-	.desc {
-		color: #606266;
-		font-size: 12px;
-		text-overflow: ellipsis;
-		overflow: hidden;
-		white-space: nowrap;
-		width: 0;
-		flex: 1;
-	}
-	.name {
-		font-weight: bold;
-		color: #303133;
-		text-overflow: ellipsis;
-		overflow: hidden;
-		white-space: nowrap;
-		width: 0;
-		flex: 1;
-		font-size: 16px;
-	}
-	.flex {
-		display: flex;
-		height: 18px;
-	}
-	.l1 {
-		height: 25px;
-	}
-	.rawname {
-		color: #909399;
-		font-size: 14px;
-	}
+.root {
+	padding: 0 15px;
+	transition: background-color 0.3s;
+	cursor: default;
+}
+.root:hover {
+	background-color: #f2f6fc;
+}
+div.entry {
+	padding: 10px 0;
+	height: 50px;
+	display: flex;
+	align-items: center;
+	border-bottom: 1px solid #e4e7ed;
+}
+.left {
+	width: max-content;
+	float: left;
+	padding-top: 5px;
+}
+.right {
+	margin-left: 15px;
+	width: 100%;
+}
+a {
+	text-decoration: none;
+}
+.desc {
+	color: #606266;
+	font-size: 12px;
+	text-overflow: ellipsis;
+	overflow: hidden;
+	white-space: nowrap;
+	width: 0;
+	flex: 1;
+}
+.name {
+	font-weight: bold;
+	color: #303133;
+	text-overflow: ellipsis;
+	overflow: hidden;
+	white-space: nowrap;
+	width: 0;
+	flex: 1;
+	font-size: 16px;
+}
+.flex {
+	display: flex;
+	height: 18px;
+}
+.l1 {
+	height: 25px;
+}
+.rawname {
+	color: #909399;
+	font-size: 14px;
+}
 </style>
