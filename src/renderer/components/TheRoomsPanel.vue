5ba6aab1edbc280d5f9e4a00efc98ba1b2957107
Icalingua <Icalinguaplusplus@protonmail.com>
2021-05-17 22:02:19 +0800

    拖拽图片文件将添加到发送框

--- src/renderer/components/TheRoomsPanel.vue
+++ src/renderer/components/TheRoomsPanel.vue
@@ -1,71 +1,75 @@
 <template>
-  <div class="root">
-    <div class="head">
-      <el-input
-        v-model="input"
-        placeholder="Search"
-        prefix-icon="el-icon-search"
-        class="input"
-      />
-    </div>
-    <div class="content">
-      <RoomEntry
-        v-for="room in sortedRooms"
-        :key="room.roomId"
-        :room="room"
-        :selected="room === selected"
-        :priority="priority"
-        @click="$emit('chroom', room)"
-        @contextmenu="$emit('contextmenu', room)"
-      />
-    </div>
-  </div>
+	<div class="root">
+		<div class="head">
+			<el-input
+				v-model="input"
+				placeholder="Search"
+				prefix-icon="el-icon-search"
+				class="input"
+			/>
+		</div>
+		<div class="content">
+			<RoomEntry
+				v-for="room in sortedRooms"
+				:key="room.roomId"
+				:room="room"
+				:selected="room === selected"
+				:priority="priority"
+				@click="$emit('chroom', room)"
+				@contextmenu="$emit('contextmenu', room)"
+			/>
+		</div>
+	</div>
 </template>
 
 <script>
 import RoomEntry from "./RoomEntry.vue";
+
 export default {
-  name: "TheRoomsPanel",
-  components: { RoomEntry },
-  computed: {
-    sortedRooms() {
-      this.input = this.input.toUpperCase();
-      let tmpr = [...this.rooms];
-      if (this.input)
-        tmpr = tmpr.filter(
-          (e) =>
-            e.roomName.toUpperCase().includes(this.input) ||
-            String(e.roomId).includes(this.input)
-        );
-      return tmpr.sort((a, b) => b.index - a.index);
-    },
-  },
-  props: ["rooms", "selected", "priority"],
-  data() {
-    return {
-      input: "",
-    };
-  },
+	name: "TheRoomsPanel",
+	components: {RoomEntry},
+	computed: {
+		sortedRooms() {
+			this.input = this.input.toUpperCase();
+			let tmpr = [...this.rooms];
+			if (this.input)
+				tmpr = tmpr.filter(
+					(e) =>
+						e.roomName.toUpperCase().includes(this.input) ||
+						String(e.roomId).includes(this.input)
+				);
+			return tmpr.sort((a, b) => b.index - a.index);
+		},
+	},
+	props: ["rooms", "selected", "priority"],
+	data() {
+		return {
+			input: "",
+		};
+	},
 };
 </script>
 
 <style scoped>
 .root {
-  border-right: 1px solid #e1e4e8;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
+	border-right: 1px solid #e1e4e8;
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
 }
+
 div.head {
-  height: 64px;
-  min-height: 64px;
-  display: flex;
-  align-items: center;
+	height: 64px;
+	min-height: 64px;
+	display: flex;
+	align-items: center;
 }
+
 .content {
-  overflow: auto;
+	overflow: auto;
 }
+
 .input {
-  margin: 0 12px;
+	margin: 0 12px;
 }
 </style>
