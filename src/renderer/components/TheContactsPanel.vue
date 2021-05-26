9f8005d46f747836eb132bcc0f7704adee626474
Clansty <i@gao4.pw>
2021-05-26 14:01:16 +0800

    通讯录搜索实现

--- src/renderer/components/TheContactsPanel.vue
+++ src/renderer/components/TheContactsPanel.vue
@@ -1,160 +1,110 @@
 <template>
-  <div class="root">
-    <el-input
-      placeholder="Enter Name"
-      v-model="searchContext"
-      @change="searchContextChanged"
-    ></el-input>
+	<div class="contacts-root">
+		<el-input
+			placeholder="Search Name"
+			v-model="searchContext"
+			style="margin-top:5px"
+		/>
 
-    <el-tabs v-model="activeName" :stretch="true" v-if="!showSearchResults">
-      <el-tab-pane label="Friends" name="friends">
-        <div
-          class="panel"
-          v-show="activeName == 'friends'"
-          v-infinite-scroll="loadF"
-          :infinite-scroll-disabled="friendsLoaded"
-          :infinite-scroll-distance="20"
-        >
-          <ContactEntry
-            v-for="i in friends"
-            :key="i.user_id"
-            :id="i.user_id"
-            :remark="i.remark"
-            :name="i.nickname"
-            @dblclick="$emit('dblclick', i.user_id, i.remark)"
-          />
-        </div>
-      </el-tab-pane>
-      <el-tab-pane label="Groups" name="groups">
-        <div
-          class="panel"
-          v-show="activeName == 'groups'"
-          v-infinite-scroll="loadG"
-          :infinite-scroll-disabled="groupsLoaded"
-          :infinite-scroll-distance="20"
-        >
-          <ContactEntry
-            v-for="i in groups"
-            :key="i.group_id"
-            :id="-i.group_id"
-            :remark="i.group_name"
-            @dblclick="$emit('dblclick', -i.group_id, i.group_name)"
-          />
-        </div>
-      </el-tab-pane>
-    </el-tabs>
-
-    <el-tabs v-model="activeName" :stretch="true" v-if="showSearchResults">
-      <el-tab-pane label="Friends" name="friends">
-        <div class="panel" v-show="activeName == 'friends'">
-          <ContactEntry
-            v-for="i in searchedFriends"
-            :key="i.user_id"
-            :id="i.user_id"
-            :remark="i.remark"
-            :name="i.nickname"
-            @dblclick="$emit('dblclick', i.user_id, i.remark)"
-          />
-        </div>
-      </el-tab-pane>
-      <el-tab-pane label="Groups" name="groups">
-        <div class="panel" v-show="activeName == 'groups'">
-          <ContactEntry
-            v-for="i in searchedGroups"
-            :key="i.group_id"
-            :id="-i.group_id"
-            :remark="i.group_name"
-            @dblclick="$emit('dblclick', -i.group_id, i.group_name)"
-          />
-        </div>
-      </el-tab-pane>
-    </el-tabs>
-  </div>
+		<el-tabs v-model="activeName" :stretch="true">
+			<el-tab-pane label="Friends" name="friends">
+				<div
+					class="contacts-panel"
+					v-show="activeName === 'friends'"
+				>
+					<ContactEntry
+						v-for="i in friendsAll"
+						:key="i.user_id"
+						:id="i.user_id"
+						:remark="i.remark"
+						:name="i.nickname"
+						v-show="i.sc.includes(searchContext)"
+						@dblclick="$emit('dblclick', i.user_id, i.remark)"
+					/>
+				</div>
+			</el-tab-pane>
+			<el-tab-pane label="Groups" name="groups">
+				<div
+					class="contacts-panel"
+					v-show="activeName === 'groups'"
+				>
+					<ContactEntry
+						v-for="i in groupsAll"
+						:key="i.group_id"
+						:id="-i.group_id"
+						:remark="i.group_name"
+						v-show="i.sc.includes(searchContext)"
+						@dblclick="$emit('dblclick', -i.group_id, i.group_name)"
+					/>
+				</div>
+			</el-tab-pane>
+		</el-tabs>
+	</div>
 </template>
 
 <script>
-import { remote } from "electron";
+import {remote} from "electron";
 import ContactEntry from "./ContactEntry.vue";
 
 const bot = remote.getGlobal("bot");
 
 export default {
-  components: { ContactEntry },
-  data() {
-    return {
-      activeName: "friends",
-      groups: [],
-      friends: [],
-      groupsAll: [],
-      friendsAll: [],
-      friendsLoaded: false,
-      groupsLoaded: false,
-      searchContext: "",
-      showSearchResults: false,
-      searchedFriends: [],
-      searchedGroups: [],
-    };
-  },
-  created() {
-    const friends = bot.getFriendList().data.values();
-    let t = friends.next();
-    while (!t.done) {
-      this.friendsAll.push(t.value);
-      t = friends.next();
-    }
-    const groups = bot.getGroupList().data.values();
-    t = groups.next();
-    while (!t.done) {
-      this.groupsAll.push(t.value);
-      t = groups.next();
-    }
-    this.groups.push(...this.groupsAll.slice(0, 20));
-    this.groupsAll = this.groupsAll.slice(20);
-    this.friends.push(...this.friendsAll.slice(0, 20));
-    this.friendsAll = this.friendsAll.slice(20);
-  },
-  methods: {
-    loadF() {
-      this.friends.push(...this.friendsAll.slice(0, 10));
-      this.friendsAll = this.friendsAll.slice(10);
-      this.friendsLoaded = this.friendsAll.length == 0;
-    },
-    loadG() {
-      this.groups.push(...this.groupsAll.slice(0, 10));
-      this.groupsAll = this.groupsAll.slice(10);
-      this.groupsLoaded = this.groupsAll.length == 0;
-    },
-    searchFriends(value) {
-      this.searchedFriends = this.friendsAll.filter((friend) => {
-        return friend.nickname.indexOf(value) != -1;
-      });
-    },
-    searchGroups(value) {
-      this.searchedGroups = this.groupsAll.filter((group) => {
-        return group.group_name.indexOf(value) != -1;
-      });
-    },
-    searchContextChanged(value) {
-      if (value != "") {
-        this.showSearchResults = true;
-      } else {
-        this.showSearchResults = false;
-      }
-      this.searchFriends(value);
-      this.searchGroups(value);
-    },
-  },
+	components: {ContactEntry},
+	data() {
+		return {
+			activeName: "friends",
+			groupsAll: [],
+			friendsAll: [],
+			searchContextEdit: "",
+		};
+	},
+	computed: {
+		searchContext : {
+			get(){
+				return this.searchContextEdit;
+			},
+			set(val){
+				this.searchContextEdit = val.toUpperCase();
+			}
+		}
+	},
+	created() {
+		setTimeout(() => {
+			const friends = bot.fl.values();
+			let t = friends.next();
+			const friendsAll = []
+			while (!t.done) {
+				const f = {...t.value}
+				//这样搜索的时候就不需要三个值里判断了
+				f.sc = (f.nickname + f.remark + f.user_id).toUpperCase()
+				friendsAll.push(f);
+				t = friends.next();
+			}
+			const groups = bot.gl.values();
+			t = groups.next();
+			const groupsAll = []
+			while (!t.done) {
+				const f = {...t.value}
+				f.sc = (f.group_name + f.group_id).toUpperCase()
+				groupsAll.push(f);
+				t = groups.next();
+			}
+
+			this.friendsAll = Object.freeze(friendsAll)
+			this.groupsAll = Object.freeze(groupsAll)
+		}, 0)
+	},
 };
 </script>
 
-<style scoped>
-.root {
-  overflow-y: auto;
-  height: 100vh;
+<style>
+.contacts-root {
+	overflow-y: auto;
+	height: 100vh;
 }
 
-.panel {
-  overflow-y: auto;
-  height: calc(100vh - 54px);
+.contacts-panel {
+	overflow-y: auto;
+	height: calc(100vh - 100px);
 }
 </style>
