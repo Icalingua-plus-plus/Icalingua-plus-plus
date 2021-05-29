6100a61d43d8359ef90dc37b0a28dccb3b263771
Icalingua <Icalinguaplusplus@protonmail.com>
2021-05-29 16:22:56 +0800

    save

--- src/renderer/interfaces/StorageProvider.ts
+++ src/renderer/interfaces/StorageProvider.ts
@@ -2,6 +2,19 @@ import Room from "./Room";
 import Message from "./Message";
 
 export default interface StorageProvider {
-    updateRoom(room: Room): void
-    addMessage(roomId: string, message:Message):void
+    connect():Promise<void>
+
+    updateRoom(roomId: string, room: object): void
+
+    addMessage(roomId: string, message: object): void
+
+    addRoom(room: object): void
+
+    removeRoom(roomId: string): void
+
+    updateMessage(roomId: string, messageId: string, message: object): void
+
+    fetchMessages(roomId: string, skip: number, limit: number): Promise<Message[]>
+
+    getAllRooms(): Promise<Room[]>
 }
