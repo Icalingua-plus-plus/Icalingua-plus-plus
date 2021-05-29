a269eb6dbd156c83e8bce1daf1fb95f9a349aaf7
Clansty <i@gao4.pw>
2021-05-29 11:46:04 +0800

    Room 接口

---
+++ src/renderer/interfaces/Room.ts
@@ -0,0 +1,17 @@
+export default interface Room {
+    roomId: Number,
+    roomName: string,
+    avatar: string,
+    index: number,
+    unreadCount: number,
+    priority: number,
+    utime: number,
+    users: [{ _id: 1, username: '1', }, { _id: 2, username: '2' }] |
+        [{ _id: 1, username: '1', }, { _id: 2, username: '2', }, { _id: 3, username: '3' }],
+    at: boolean,
+    lastMessage: {
+        content: string,
+        timestamp: string,
+        username: string | undefined
+    }
+}
