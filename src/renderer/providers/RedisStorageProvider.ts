3b9041ae137a790b42cce10584cfb5c6d2c1f1ac
Clansty <i@gao4.pw>
2021-06-10 21:58:42 +0800

    修复 room 排序

--- src/renderer/providers/RedisStorageProvider.ts
+++ src/renderer/providers/RedisStorageProvider.ts
@@ -2,82 +2,90 @@ import * as Redis from "ioredis";
 import Message from "../interfaces/Message";
 import Room from "../interfaces/Room";
 import StorageProvider from "../interfaces/StorageProvider";
+
 export default class RedisStorageProvider implements StorageProvider {
-  qid: string;
-  connStr: string;
-  redis: Redis.Redis;
+    qid: string;
+    connStr: string;
+    redis: Redis.Redis;
 
-  constructor(connStr: string, id: string) {
-    this.connStr = connStr;
-    this.qid = `eqq:${id}`;
-  }
+    constructor(connStr: string, id: string) {
+        this.connStr = connStr;
+        this.qid = `eqq:${id}`;
+    }
 
-  connect(): Promise<void> {
-    this.redis = new Redis(this.connStr);
-    return Promise.resolve();
-  }
-  updateRoom(roomId: number, room: object): void {
-    this.redis.set(`${this.qid}:rooms:${roomId}`, JSON.stringify(room));
-  }
-  addRoom(room: Room): void {
-    this.redis.set(`${this.qid}:rooms:${room.roomId}`, JSON.stringify(room));
-  }
-  removeRoom(roomId: number): void {
-    this.redis.del(`${this.qid}:rooms:${roomId}`);
-  }
-  async getAllRooms(): Promise<Room[]> {
-    const roomKeys = await this.redis.keys(`${this.qid}:rooms*`);
-    const roomsPAry = roomKeys.map(async (key) => {
-      const room = await this.redis.get(key);
-      return JSON.parse(room) as Room;
-    });
-    const rooms = await Promise.all(roomsPAry);
-    return rooms;
-  }
+    connect(): Promise<void> {
+        this.redis = new Redis(this.connStr);
+        return Promise.resolve();
+    }
 
-  addMessage(roomId: number, message: Message): void {
-    this.redis.set(
-      `${this.qid}:msgof${roomId}:${message._id}`,
-      JSON.stringify(message)
-    );
-  }
-  updateMessage(roomId: number, messageId: string, message: object): void {
-    this.redis.set(
-      `${this.qid}:msgof${roomId}:${messageId}`,
-      JSON.stringify(message)
-    );
-  }
-  async fetchMessages(
-    roomId: number,
-    skip: number,
-    limit: number
-  ): Promise<Message[]> {
-    const msgKeys = await this.redis.keys(`${this.qid}:msgof${roomId}:*`);
-    const msgtoFetchKeys = msgKeys.slice(
-      msgKeys.length - 1 - skip - limit,
-      msgKeys.length - skip
-    );
-    const messagesPAry = msgtoFetchKeys.map(async (key) => {
-      const msg = await this.redis.get(key);
-      return JSON.parse(msg) as Message;
-    });
-    const messages = await Promise.all(messagesPAry);
-    return messages;
-  }
+    updateRoom(roomId: number, room: object): void {
+        this.redis.set(`${this.qid}:rooms:${roomId}`, JSON.stringify(room));
+    }
 
-  async getMessage(roomId: number, messageId: string): Promise<Message> {
-    const msgString = await this.redis.get(
-      `${this.qid}:msgof${roomId}:${messageId}`
-    );
-    return JSON.parse(msgString);
-  }
-  addMessages(roomId: number, messages: Message[]): Promise<any> {
-    messages.forEach((message) => {
-      this.redis.set(
-        `${this.qid}:msgof${roomId}:${message._id}`,
-        JSON.stringify(message)
-      );
-    });
-    return Promise.resolve();
-  }
+    addRoom(room: Room): void {
+        this.redis.set(`${this.qid}:rooms:${room.roomId}`, JSON.stringify(room));
+    }
+
+    removeRoom(roomId: number): void {
+        this.redis.del(`${this.qid}:rooms:${roomId}`);
+    }
+
+    async getAllRooms(): Promise<Room[]> {
+        const roomKeys = await this.redis.keys(`${this.qid}:rooms*`);
+        const roomsPAry = roomKeys.map(async (key) => {
+            const room = await this.redis.get(key);
+            return JSON.parse(room) as Room;
+        });
+        const rooms = await Promise.all(roomsPAry);
+        return rooms.sort(e => -e.utime);
+    }
+
+    addMessage(roomId: number, message: Message): void {
+        this.redis.set(
+            `${this.qid}:msgof${roomId}:${message._id}`,
+            JSON.stringify(message)
+        );
+    }
+
+    updateMessage(roomId: number, messageId: string, message: object): void {
+        this.redis.set(
+            `${this.qid}:msgof${roomId}:${messageId}`,
+            JSON.stringify(message)
+        );
+    }
+
+    async fetchMessages(
+        roomId: number,
+        skip: number,
+        limit: number
+    ): Promise<Message[]> {
+        const msgKeys = await this.redis.keys(`${this.qid}:msgof${roomId}:*`);
+        const msgtoFetchKeys = msgKeys.slice(
+            msgKeys.length - 1 - skip - limit,
+            msgKeys.length - skip
+        );
+        const messagesPAry = msgtoFetchKeys.map(async (key) => {
+            const msg = await this.redis.get(key);
+            return JSON.parse(msg) as Message;
+        });
+        const messages = await Promise.all(messagesPAry);
+        return messages;
+    }
+
+    async getMessage(roomId: number, messageId: string): Promise<Message> {
+        const msgString = await this.redis.get(
+            `${this.qid}:msgof${roomId}:${messageId}`
+        );
+        return JSON.parse(msgString);
+    }
+
+    addMessages(roomId: number, messages: Message[]): Promise<any> {
+        messages.forEach((message) => {
+            this.redis.set(
+                `${this.qid}:msgof${roomId}:${message._id}`,
+                JSON.stringify(message)
+            );
+        });
+        return Promise.resolve();
+    }
 }
