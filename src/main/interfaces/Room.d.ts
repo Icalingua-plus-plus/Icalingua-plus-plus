224c92a76c9261f19595e32cb1603d9321af4f76
Icalingua <Icalinguaplusplus@protonmail.com>
2021-06-28 12:41:03 +0800

    在 ipc 中初始化数据库

---
+++ src/main/interfaces/Room.d.ts
@@ -0,0 +1,24 @@
+export default interface Room {
+  roomId: Number;
+  roomName: string;
+  avatar: string;
+  index: number;
+  unreadCount: number;
+  priority: number;
+  utime: number;
+  users:
+    | [{ _id: 1; username: "1" }, { _id: 2; username: "2" }]
+    | [
+        { _id: 1; username: "1" },
+        { _id: 2; username: "2" },
+        { _id: 3; username: "3" }
+      ];
+  at: boolean | null;
+  lastMessage: {
+    content: string;
+    timestamp: string;
+    username?: string;
+  };
+  autoDownload?: boolean;
+  downloadPath?: string;
+}
