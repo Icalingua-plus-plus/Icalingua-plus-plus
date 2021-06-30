ad1599f0d78d3a373c8b6d4567def0cd12939f54
Icalingua <Icalinguaplusplus@protonmail.com>
2021-06-30 16:26:20 +0800

    清除旧的代码

---
+++ src/renderer/views/Aria2Settings.vue
@@ -0,0 +1,37 @@
+<template>
+	<div>
+		<el-form v-model="aria2" label-width="100px">
+			<el-form-item label="enabled">
+				<el-switch v-model="aria2.enabled"/>
+			</el-form-item>
+			<el-form-item label="host">
+				<el-input v-model="aria2.host"/>
+			</el-form-item>
+			<el-form-item label="port">
+				<el-input-number v-model.number="aria2.port"/>
+			</el-form-item>
+			<el-form-item label="secure">
+				<el-switch v-model="aria2.secure"/>
+			</el-form-item>
+			<el-form-item label="secret">
+				<el-input v-model="aria2.secret"/>
+			</el-form-item>
+			<el-form-item label="path">
+				<el-input v-model="aria2.path"/>
+			</el-form-item>
+		</el-form>
+		<div slot="footer" class="dialog-footer">
+			<el-button type="primary" @click="closeAria">Close</el-button>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Aria2Settings',
+}
+</script>
+
+<style scoped>
+
+</style>
