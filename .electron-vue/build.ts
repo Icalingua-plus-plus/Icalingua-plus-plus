6a19882955c0164efce86c1dcda9aa68d07210ae
Clansty <i@gao4.pw>
2021-06-30 23:47:05 +0800

    fix build

--- .electron-vue/build.ts
+++ .electron-vue/build.ts
@@ -12,7 +12,6 @@ const Multispinner = require('multispinner')
 
 const mainConfig = require('./webpack.main.config')
 const rendererConfig = require('./webpack.renderer.config')
-const webConfig = require('./webpack.web.config')
 
 const doneLog = chalk.bgGreen.white(' DONE ') + ' '
 const errorLog = chalk.bgRed.white(' ERROR ') + ' '
@@ -20,7 +19,6 @@ const okayLog = chalk.bgBlue.white(' OKAY ') + ' '
 const isCI = process.env.CI || false
 
 if (process.env.BUILD_TARGET === 'clean') clean()
-else if (process.env.BUILD_TARGET === 'web') web()
 else build()
 
 function clean () {
@@ -95,21 +93,6 @@ function pack (config) {
         }))
       }
     })
-  })
-}
-
-function web () {
-  del.sync(['dist/web/*', '!.gitkeep'])
-  webConfig.mode = 'production'
-  webpack(webConfig, (err, stats) => {
-    if (err || stats.hasErrors()) console.log(err)
-
-    console.log(stats.toString({
-      chunks: false,
-      colors: true
-    }))
-
-    process.exit()
   })
 }
 
