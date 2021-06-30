4d894524bc78cb96b6f7e37c6bf80b3f93a90b73
Icalingua <Icalinguaplusplus@protonmail.com>
2021-06-30 23:03:29 +0800

    index.dev 也 ts 化

--- src/main/index.dev.ts
+++ src/main/index.dev.ts
@@ -11,14 +11,14 @@
 //require('electron-debug')()
 
 // Install `vue-devtools`
-require('electron').app.on('ready', () => {
-  let installExtension = require('electron-devtools-installer')
-  installExtension.default(installExtension.VUEJS_DEVTOOLS)
-    .then(() => {})
-    .catch(err => {
-      console.log('Unable to install `vue-devtools`: \n', err)
-    })
-})
+// require('electron').app.on('ready', () => {
+//   let installExtension = require('electron-devtools-installer')
+//   installExtension.default(installExtension.VUEJS_DEVTOOLS)
+//     .then(() => {})
+//     .catch(err => {
+//       console.log('Unable to install `vue-devtools`: \n', err)
+//     })
+// })
 
 // Require `main` process to boot app
 require('./index')
