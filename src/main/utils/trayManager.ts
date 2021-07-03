abaf275cae0aa51c19d6a716d190335fc06ca3bd
Icalingua <Icalinguaplusplus@protonmail.com>
2021-07-03 22:04:42 +0800

    ctrl w 关闭窗口

--- src/main/utils/trayManager.ts
+++ src/main/utils/trayManager.ts
@@ -6,11 +6,16 @@ import {getFirstUnreadRoom, getUnreadCount} from '../ipc/botAndStorage'
 import {getConfig} from './configManager'
 import getStaticPath from '../../utils/getStaticPath'
 import {pushUnreadCount} from './socketIoSlave'
+import openImage from '../ipc/openImage'
+import setPriority from './setPriority'
 
 let tray: Tray
 
 export const createTray = () => {
-    tray = new Tray(path.join(getStaticPath(), '/256x256.png'))
+    tray = new Tray(path.join(
+        getStaticPath(),
+        getConfig().darkTaskIcon ? 'darknewmsg.png' : 'newmsg.png',
+    ))
     tray.setToolTip('Electron QQ')
     tray.on('click', () => {
         const window = getMainWindow()
@@ -29,12 +34,50 @@ export const createTray = () => {
                 },
             },
             {
+                label: '通知优先级',
+                submenu: [
+                    {
+                        type: 'radio',
+                        label: '1',
+                        checked: getConfig().priority === 1,
+                        click: () => setPriority(1),
+                    },
+                    {
+                        type: 'radio',
+                        label: '2',
+                        checked: getConfig().priority === 2,
+                        click: () => setPriority(2),
+                    },
+                    {
+                        type: 'radio',
+                        label: '3',
+                        checked: getConfig().priority === 3,
+                        click: () => setPriority(3),
+                    },
+                    {
+                        type: 'radio',
+                        label: '4',
+                        checked: getConfig().priority === 4,
+                        click: () => setPriority(4),
+                    },
+                    {
+                        type: 'radio',
+                        label: '5',
+                        checked: getConfig().priority === 5,
+                        click: () => setPriority(5),
+                    },
+                ],
+            },
+            {
                 label: 'Exit',
                 type: 'normal',
                 click: exit,
             },
         ]),
     )
+
+}
+export const updateTrayMenu=()=>{
 
 }
 export const updateTrayIcon = async (roomName?: string) => {
