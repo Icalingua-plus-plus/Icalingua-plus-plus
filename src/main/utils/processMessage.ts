8a910c30ee55dd2bfe7603c30d14369beccbef74
Clansty <i@gao4.pw>
2021-07-18 23:12:48 +0800

    添加 mirai 解析

--- src/main/utils/processMessage.ts
+++ src/main/utils/processMessage.ts
@@ -1,5 +1,5 @@
-import {AtElem, FriendInfo, GroupMessageEventData, MemberBaseInfo, MessageElem} from "oicq";
-import Message from "../../types/Message";
+import {AtElem, FriendInfo, GroupMessageEventData, MemberBaseInfo, MessageElem} from 'oicq'
+import Message from '../../types/Message'
 import oicq from '../adapters/oicqAdapter'
 import {base64decode} from 'nodejs-base64'
 import mime from './mime'
@@ -10,65 +10,65 @@ const processMessage = async (oicqMessage: MessageElem[], message: Message, last
         oicqMessage = [oicqMessage]
     let lastType
     for (let i = 0; i < oicqMessage.length; i++) {
-        const m = oicqMessage[i];
-        let appurl;
-        let url;
+        const m = oicqMessage[i]
+        let appurl
+        let url
         switch (m.type) {
-            case "at":
+            case 'at':
                 if (lastType === 'reply')
                     break
-            case "text":
-                lastMessage.content += m.data.text;
-                message.content += m.data.text;
-                if ((m as AtElem).data.qq === "all") {
-                    message.at = "all";
+            case 'text':
+                lastMessage.content += m.data.text
+                message.content += m.data.text
+                if ((m as AtElem).data.qq === 'all') {
+                    message.at = 'all'
                 } else if ((m as AtElem).data.qq == oicq.getUin()) {
-                    message.at = true;
+                    message.at = true
                 }
-                break;
-            case "image":
-            case "flash":
-                lastMessage.content += "[Image]";
-                url = m.data.url;
+                break
+            case 'image':
+            case 'flash':
+                lastMessage.content += '[Image]'
+                url = m.data.url
                 message.file = {
-                    type: "image/jpeg",
+                    type: 'image/jpeg',
                     url,
-                };
-                break;
-            case "bface":
-                lastMessage.content += "[Sticker]" + m.data.text;
+                }
+                break
+            case 'bface':
+                lastMessage.content += '[Sticker]' + m.data.text
                 url = `https://gxh.vip.qq.com/club/item/parcel/item/${m.data.file.substr(
                     0,
-                    2
-                )}/${m.data.file.substr(0, 32)}/300x300.png`;
+                    2,
+                )}/${m.data.file.substr(0, 32)}/300x300.png`
                 message.file = {
-                    type: "image/webp",
+                    type: 'image/webp',
                     url,
-                };
-                break;
-            case "file":
-                lastMessage.content += "[File]" + m.data.name;
-                message.content += m.data.name;
+                }
+                break
+            case 'file':
+                lastMessage.content += '[File]' + m.data.name
+                message.content += m.data.name
                 message.file = {
                     type: mime(path.extname(m.data.name)),
                     size: m.data.size,
                     url: m.data.url,
                     name: m.data.name,
-                    fid: m.data.fileid
-                };
-                break;
-            case "share":
-                lastMessage.content += "[Link]" + m.data.title;
-                message.content += m.data.url;
-                break;
-            case "reply":
+                    fid: m.data.fileid,
+                }
+                break
+            case 'share':
+                lastMessage.content += '[Link]' + m.data.title
+                message.content += m.data.url
+                break
+            case 'reply':
                 let replyMessage: Message
                 if (roomId) {
-                    replyMessage = await oicq.getMessageFromStorage(roomId, m.data.id);
+                    replyMessage = await oicq.getMessageFromStorage(roomId, m.data.id)
                 }
                 if (!replyMessage) {
                     //get the message
-                    const getRet = await oicq.getMsg(m.data.id);
+                    const getRet = await oicq.getMsg(m.data.id)
                     if (getRet.data) {
                         //获取到库里面还没有的历史消息
                         //暂时先不加回库里了
@@ -77,13 +77,13 @@ const processMessage = async (oicqMessage: MessageElem[], message: Message, last
                             ? (data as GroupMessageEventData).anonymous
                                 ? (data as GroupMessageEventData).anonymous.name
                                 : oicq.getUin() === data.sender.user_id
-                                    ? "You"
+                                    ? 'You'
                                     : (data.sender as MemberBaseInfo).card || data.sender.nickname
                             : (data.sender as FriendInfo).remark || data.sender.nickname
                         replyMessage = {
-                            _id: "", date: "", senderId: 0, timestamp: "",
+                            _id: '', date: '', senderId: 0, timestamp: '',
                             username: senderName,
-                            content: ''
+                            content: '',
                         }
                         await processMessage(data.message, replyMessage, {})
                     }
@@ -93,15 +93,15 @@ const processMessage = async (oicqMessage: MessageElem[], message: Message, last
                         _id: m.data.id,
                         username: replyMessage.username,
                         content: replyMessage.content,
-                    };
+                    }
                     if (replyMessage.file) {
-                        message.replyMessage.file = replyMessage.file;
+                        message.replyMessage.file = replyMessage.file
                     }
                 }
-                break;
-            case "json":
-                const json: string = m.data.data;
-                message.code = json;
+                break
+            case 'json':
+                const json: string = m.data.data
+                message.code = json
                 const jsonObj = JSON.parse(json)
                 if (jsonObj.app === 'com.tencent.mannounce') {
                     try {
@@ -113,72 +113,78 @@ const processMessage = async (oicqMessage: MessageElem[], message: Message, last
                     } catch (err) {
                     }
                 }
-                const biliRegex = /(https?:\\?\/\\?\/b23\.tv\\?\/\w*)\??/;
-                const zhihuRegex = /(https?:\\?\/\\?\/\w*\.?zhihu\.com\\?\/[^?"=]*)\??/;
-                const biliRegex2 = /(https?:\\?\/\\?\/\w*\.?bilibili\.com\\?\/[^?"=]*)\??/;
-                const jsonLinkRegex = /{.*"app":"com.tencent.structmsg".*"jumpUrl":"(https?:\\?\/\\?\/[^",]*)".*}/;
-                const jsonAppLinkRegex = /"contentJumpUrl": ?"(https?:\\?\/\\?\/[^",]*)"/;
+                const biliRegex = /(https?:\\?\/\\?\/b23\.tv\\?\/\w*)\??/
+                const zhihuRegex = /(https?:\\?\/\\?\/\w*\.?zhihu\.com\\?\/[^?"=]*)\??/
+                const biliRegex2 = /(https?:\\?\/\\?\/\w*\.?bilibili\.com\\?\/[^?"=]*)\??/
+                const jsonLinkRegex = /{.*"app":"com.tencent.structmsg".*"jumpUrl":"(https?:\\?\/\\?\/[^",]*)".*}/
+                const jsonAppLinkRegex = /"contentJumpUrl": ?"(https?:\\?\/\\?\/[^",]*)"/
                 if (biliRegex.test(json))
-                    appurl = json.match(biliRegex)[1].replace(/\\\//g, "/");
+                    appurl = json.match(biliRegex)[1].replace(/\\\//g, '/')
                 else if (biliRegex2.test(json))
-                    appurl = json.match(biliRegex2)[1].replace(/\\\//g, "/");
+                    appurl = json.match(biliRegex2)[1].replace(/\\\//g, '/')
                 else if (zhihuRegex.test(json))
-                    appurl = json.match(zhihuRegex)[1].replace(/\\\//g, "/");
+                    appurl = json.match(zhihuRegex)[1].replace(/\\\//g, '/')
                 else if (jsonLinkRegex.test(json))
-                    appurl = json.match(jsonLinkRegex)[1].replace(/\\\//g, "/");
+                    appurl = json.match(jsonLinkRegex)[1].replace(/\\\//g, '/')
                 else if (jsonAppLinkRegex.test(json))
-                    appurl = json.match(jsonAppLinkRegex)[1].replace(/\\\//g, "/");
+                    appurl = json.match(jsonAppLinkRegex)[1].replace(/\\\//g, '/')
                 if (appurl) {
-                    lastMessage.content = appurl;
-                    message.content = appurl;
+                    lastMessage.content = appurl
+                    message.content = appurl
                 } else {
-                    lastMessage.content = "[JSON]";
-                    message.content = "[JSON]";
+                    lastMessage.content = '[JSON]'
+                    message.content = '[JSON]'
                 }
-                break;
-            case "xml":
-                message.code = m.data.data;
-                const urlRegex = /url="([^"]+)"/;
+                break
+            case 'xml':
+                message.code = m.data.data
+                const urlRegex = /url="([^"]+)"/
                 if (urlRegex.test(m.data.data))
-                    appurl = m.data.data.match(urlRegex)[1].replace(/\\\//g, "/");
+                    appurl = m.data.data.match(urlRegex)[1].replace(/\\\//g, '/')
                 if (m.data.data.includes('action="viewMultiMsg"')) {
-                    lastMessage.content += "[Forward multiple messages]";
-                    message.content += "[Forward multiple messages]";
-                    const resIdRegex = /m_resid="([\w+=/]+)"/;
+                    lastMessage.content += '[Forward multiple messages]'
+                    message.content += '[Forward multiple messages]'
+                    const resIdRegex = /m_resid="([\w+=/]+)"/
                     if (resIdRegex.test(m.data.data)) {
-                        const resId = m.data.data.match(resIdRegex)[1];
-                        console.log(resId);
-                        message.content = `[Forward: ${resId}]`;
+                        const resId = m.data.data.match(resIdRegex)[1]
+                        console.log(resId)
+                        message.content = `[Forward: ${resId}]`
                     }
                 } else if (appurl) {
-                    appurl = appurl.replace(/&amp;/g, "&");
-                    lastMessage.content = appurl;
-                    message.content = appurl;
+                    appurl = appurl.replace(/&amp;/g, '&')
+                    lastMessage.content = appurl
+                    message.content = appurl
                 } else {
-                    lastMessage.content += "[XML]";
-                    message.content += "[XML]";
+                    lastMessage.content += '[XML]'
+                    message.content += '[XML]'
                 }
-                break;
-            case "face":
-                message.content += `[Face: ${m.data.id}]`;
+                break
+            case 'face':
+                message.content += `[Face: ${m.data.id}]`
                 lastMessage.content += m.data.text
-                break;
-            case "video":
-                message.content = "";
-                lastMessage.content = `[Video]`;
+                break
+            case 'video':
+                message.content = ''
+                lastMessage.content = `[Video]`
                 message.file = {
-                    type: "video/mp4",
+                    type: 'video/mp4',
                     url: m.data.url,
-                };
-                break;
-            case "record":
-                message.content = "[Audio]";
-                lastMessage.content = `[Audio]`;
-                break;
+                }
+                break
+            case 'record':
+                message.content = '[Audio]'
+                lastMessage.content = '[Audio]'
+                break
+            case 'mirai':
+                try {
+                    message.mirai = JSON.parse(m.data.data)
+                } catch (e) {
+                }
+                break
         }
         lastType = m.type
     }
-    return {message, lastMessage};
+    return {message, lastMessage}
 }
 
 export default processMessage
