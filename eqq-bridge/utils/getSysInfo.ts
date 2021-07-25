d85dd74ca30fbaa0dd8c8308b5f16e9a5766989c
Clansty <i@gao4.pw>
2021-07-25 21:34:19 +0800

    主界面显示 bridge 所在机器状态

---
+++ eqq-bridge/utils/getSysInfo.ts
@@ -0,0 +1,12 @@
+import {version, name} from '../package.json'
+import os from 'os'
+
+export default () => {
+    const used = process.memoryUsage()
+    const load = os.loadavg()
+    return `${name} ${version}
+Running on ${os.type()} ${os.hostname()} ${os.release()}
+Resident Set Size ${Math.round((used.rss / 1024 / 1024) * 100) / 100}MB
+Heap used ${Math.round((used.heapUsed / 1024 / 1024) * 100) / 100}MB
+Load ${load.join(' ')}`
+}
