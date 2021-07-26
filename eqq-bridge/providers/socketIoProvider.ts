aa987c71f39af2334b7e1624ab40e545530c203a
Icalingua <Icalinguaplusplus@protonmail.com>
2021-07-26 11:35:03 +0800

    更改签名验证方式

--- eqq-bridge/providers/socketIoProvider.ts
+++ eqq-bridge/providers/socketIoProvider.ts
@@ -1,9 +1,10 @@
 import {createServer} from 'http'
 import {Server} from 'socket.io'
-import crypto from 'crypto'
+import {verify} from 'noble-ed25519'
 import {config} from './configManager'
 import adapter from '../adapters/oicqAdapter'
 import registerSocketHandlers from '../handlers/registerSocketHandlers'
+import md5 from 'md5'
 
 const httpServer = createServer()
 const io = new Server(httpServer, {
@@ -15,13 +16,10 @@ const io = new Server(httpServer, {
 io.on('connection', (socket) => {
     console.log('new client connected')
     //客户端对这个服务器发来的时间用私钥签名给服务端验证
-    const salt = new Date().getTime().toString()
+    const salt = md5(new Date().getTime().toString())
     socket.emit('requireAuth', salt)
-    socket.once('auth', (sign: string) => {
-        const verify = crypto.createVerify('RSA-SHA1')
-        verify.update(salt)
-        verify.end()
-        if (verify.verify(config.pubKey, Buffer.from(sign, 'base64'))) {
+    socket.once('auth', async (sign: string) => {
+        if (await verify(sign, salt, config.pubKey)) {
             console.log('客户端验证成功')
             socket.emit('authSucceed')
             socket.join('authed')
