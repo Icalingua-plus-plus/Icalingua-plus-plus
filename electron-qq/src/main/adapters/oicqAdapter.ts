1a2b61325e8151fb03d668c68003d44f25267796
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2021-07-27 23:09:01 +0800

    完成SQL

--- electron-qq/src/main/adapters/oicqAdapter.ts
+++ electron-qq/src/main/adapters/oicqAdapter.ts
@@ -373,7 +373,24 @@ const initStorage = async () => {
                 storage = new RedisStorageProvider(loginForm.rdsHost, `${loginForm.username}`)
                 break;
             case 'sqlite':
-                storage = new SQLStorageProvider(`${loginForm.username}`,"sqlite3")
+                storage = new SQLStorageProvider(`${loginForm.username}`, "sqlite3")
+                break;
+            case 'mysql':
+                storage = new SQLStorageProvider(`${loginForm.username}`, "mysql", {
+                    host: loginForm.sqlHost,
+                    user: loginForm.sqlUsername,
+                    password: loginForm.sqlPassword,
+                    database: loginForm.sqlDatabase,
+                })
+                break;
+            case 'pg':
+                storage = new SQLStorageProvider(`${loginForm.username}`, "pg", {
+                    host: loginForm.sqlHost,
+                    user: loginForm.sqlUsername,
+                    password: loginForm.sqlPassword,
+                    database: loginForm.sqlDatabase,
+                })
+                break;
             default:
                 break;
         }
