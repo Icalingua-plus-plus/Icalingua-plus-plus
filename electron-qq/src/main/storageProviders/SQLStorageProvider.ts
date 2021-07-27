8ffe9e03088e3f13b5b3ad746769eaff141ce42d
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2021-07-27 15:30:52 +0800

    完成PostgreSQL支持

--- electron-qq/src/main/storageProviders/SQLStorageProvider.ts
+++ electron-qq/src/main/storageProviders/SQLStorageProvider.ts
@@ -24,23 +24,28 @@ export default class SQLStorageProvider implements StorageProvider {
   ) {
     this.id = id;
     this.qid = `eqq${id}`;
+    this.type = type;
     switch (type) {
       case "sqlite3":
         this.db = knex({
           client: "sqlite3",
-          connection: { filename: this.qid },
+          connection: { filename: `${this.qid}.db` },
+          useNullAsDefault: true,
         });
         break;
       case "mysql":
         this.db = knex({
           client: "mysql",
           connection: { ...connectOpt },
+          useNullAsDefault: true,
         });
         break;
       case "pg":
         this.db = knex({
           client: "pg",
           connection: { ...connectOpt },
+          useNullAsDefault: true,
+          searchPath: [this.qid, "public"],
         });
         break;
       default:
@@ -59,7 +64,19 @@ export default class SQLStorageProvider implements StorageProvider {
   }
 
   private roomConFromDB(room: Record<string, any>): Room {
-    return room as Room;
+    if (room) return { ...room, roomId: Number(room.roomId) } as Room;
+    return null;
+  }
+
+  private msgConFromDB(message: Message): Message {
+    if (message) {
+      return {
+        ...message,
+        senderId: Number(message.senderId),
+        time: Number(message.time),
+      };
+    }
+    return null;
   }
 
   private updateDB() {
@@ -67,111 +84,109 @@ export default class SQLStorageProvider implements StorageProvider {
   }
 
   async connect(): Promise<void> {
-    await this.db.schema.createSchemaIfNotExists(`${this.qid}`);
-
     // 建表存放数据库版本以便升级，当前版本为0。
-    const hasVersionTable = await this.db.schema
-      .withSchema(this.qid)
-      .hasTable(`dbVersion`);
+    const hasVersionTable = await this.db.schema.hasTable(`dbVersion`);
     if (!hasVersionTable) {
-      await this.db.schema
-        .withSchema(this.qid)
-        .createTable(`dbVersion`, (table) => {
-          table.integer("dbVersion");
-          table.primary(["dbVersion"]);
-        });
-      await this.db(`dbVersion`)
-        .withSchema(this.qid)
-        .insert({ dbVersion: 0 });
+      await this.db.schema.createTable(`dbVersion`, (table) => {
+        table.integer("dbVersion");
+        table.primary(["dbVersion"]);
+      });
+      await this.db(`dbVersion`).insert({ dbVersion: 0 });
     }
-    const dbVersion = await this.db<{ dbVersion: number }>(`dbVersion`)
-      .withSchema(this.qid)
-      .select();
+
+    // 建表存放聊天数据库名以便升级。
+    const hasMsgTableNameTable = await this.db.schema.hasTable(`msgTableName`);
+    if (!hasMsgTableNameTable) {
+      await this.db.schema.createTable("msgTableName", (table) => {
+        table.increments("id").primary();
+        table.string("tableName");
+      });
+    }
+
+    const dbVersion = await this.db<{ dbVersion: number }>(`dbVersion`).select(
+      "*"
+    );
     // 若版本低于当前版本则启动升级函数
     if (dbVersion[0].dbVersion < 0) {
       this.updateDB();
     }
 
     // 建表存放聊天
-    const hasRoomTable = await this.db.schema
-      .withSchema(this.qid)
-      .hasTable(`rooms`);
+    const hasRoomTable = await this.db.schema.hasTable(`rooms`);
     if (!hasRoomTable) {
-      await this.db.schema
-        .withSchema(this.qid)
-        .createTable(`rooms`, (table) => {
-          table
-            .integer("roomId")
-            .unique()
-            .index()
-            .primary();
-          table.string("roomName");
-          table.string("avatar");
-          table.integer("index");
-          table.integer("unreadCount");
-          table.integer("priority");
-          table.integer("utime").index();
-          table.jsonb("users");
-          table.jsonb("lastMessage");
-        });
+      await this.db.schema.createTable(`rooms`, (table) => {
+        table
+          .string("roomId")
+          .unique()
+          .index()
+          .primary();
+        table.string("roomName");
+        table.string("avatar");
+        table.integer("index");
+        table.integer("unreadCount");
+        table.integer("priority");
+        table.bigInteger("utime").index();
+        table.jsonb("users");
+        table.jsonb("lastMessage");
+        table.boolean("at").nullable();
+        table.boolean("autoDownload").nullable();
+        table.string("downloadPath").nullable();
+      });
     }
 
     // 建表存放忽略聊天
-    const hasIgnoredTable = await this.db.schema
-      .withSchema(this.qid)
-      .hasTable(`ignoredChats`);
+    const hasIgnoredTable = await this.db.schema.hasTable(`ignoredChats`);
     if (!hasIgnoredTable) {
-      await this.db.schema
-        .withSchema(this.qid)
-        .createTable(`ignoredChats`, (table) => {
-          table
-            .integer("id")
-            .unique()
-            .primary()
-            .index();
-          table.string("name");
-        });
+      await this.db.schema.createTable(`ignoredChats`, (table) => {
+        table
+          .integer("id")
+          .unique()
+          .primary()
+          .index();
+        table.string("name");
+      });
     }
   }
 
   async addRoom(room: Room): Promise<any> {
-    await this.db(`rooms`)
-      .withSchema(this.qid)
-      .insert(this.roomConToDB(room));
+    return await this.db(`rooms`).insert(this.roomConToDB(room));
   }
 
   async updateRoom(roomId: number, room: Record<string, any>): Promise<any> {
     await this.db(`rooms`)
-      .withSchema(this.qid)
+
       .where("roomId", "=", roomId)
       .update(this.roomConToDB(room));
   }
 
   async removeRoom(roomId: number): Promise<any> {
     await this.db(`rooms`)
-      .withSchema(this.qid)
+
       .where("roomId", "=", roomId)
       .delete();
+    await this.db("msgTableName")
+
+      .where("tableName", "=", `msg${roomId}`)
+      .insert({ tableName: `msg${roomId}` });
+    await this.db.schema.dropTableIfExists(`msg${roomId}`);
   }
 
   async getAllRooms(): Promise<Room[]> {
-    const rooms = await this.db<Room>(`rooms`)
-      .withSchema(this.qid)
-      .select();
+    const rooms = await this.db<Room>(`rooms`).select("*");
     return rooms.map((room) => this.roomConFromDB(room));
   }
 
   async getRoom(roomId: number): Promise<Room> {
     const room = await this.db<Room>(`rooms`)
-      .withSchema(this.qid)
+
       .where("roomId", "=", roomId)
-      .select();
-    return this.roomConFromDB(room);
+      .select("*");
+    return this.roomConFromDB(room[0]);
   }
 
   async getUnreadCount(priority: number): Promise<number> {
     const unreadRooms = await this.db<Room>(`rooms`)
-      .withSchema(this.qid)
+
       .where("unreadCount", ">", 0)
       .where("priority", "=", priority)
       .select("roomId"); // 尽可能减小通过 node 的数据量
@@ -180,71 +195,72 @@ export default class SQLStorageProvider implements StorageProvider {
 
   async getFirstUnreadRoom(priority: number): Promise<Room> {
     const unreadRooms = await this.db<Room>(`rooms`)
-      .withSchema(this.qid)
+
       .where("unreadCount", ">", 0)
       .where("priority", "=", priority)
       .orderBy("utime", "desc")
-      .select();
+      .select("*");
     if (unreadRooms.length >= 1) return unreadRooms[0];
     return null;
   }
 
   async getIgnoredChats(): Promise<IgnoreChatInfo[]> {
-    return await this.db<IgnoreChatInfo>(`ignoredChats`)
-      .withSchema(this.qid)
-      .select();
+    return await this.db<IgnoreChatInfo>(`ignoredChats`).select("*");
   }
 
   async isChatIgnored(id: number): Promise<boolean> {
-    const ignoredChats = await this.db<IgnoreChatInfo>(`ignoredChats`)
-      .withSchema(this.qid)
-      .where("id", "=", id);
+    const ignoredChats = await this.db<IgnoreChatInfo>(`ignoredChats`).where(
+      "id",
+      "=",
+      id
+    );
     return ignoredChats.length !== 0;
   }
 
   async addIgnoredChat(info: IgnoreChatInfo): Promise<any> {
-    await this.db<IgnoreChatInfo>(`ignoredChats`)
-      .withSchema(this.qid)
-      .insert(info);
+    await this.db<IgnoreChatInfo>(`ignoredChats`).insert(info);
   }
 
   async removeIgnoredChat(roomId: number): Promise<any> {
     await this.db<IgnoreChatInfo>(`ignoredChats`)
-      .withSchema(this.qid)
+
       .where("id", "=", roomId)
       .delete();
   }
 
-  async addMessage(roomId: number, message: Message): Promise<any> {
-    // 建表存放聊天记录
-    const hasMsgTable = await this.db.schema
-      .withSchema(this.qid)
-      .hasTable(`msg${roomId}`);
+  // 建表存放聊天记录
+  private async createMsgTable(roomId: number) {
+    const hasMsgTable = await this.db.schema.hasTable(`msg${roomId}`);
     if (!hasMsgTable) {
-      await this.db.schema
-        .withSchema(this.qid)
-        .createTable(`msg${roomId}`, (table) => {
-          table
-            .string("_id")
-            .unique()
-            .index()
-            .primary()
-            .unsigned();
-          table.integer("senderId");
-          table.string("username");
-          table.string("content").nullable();
-          table.string("timestamp");
-          table.string("date");
-          table.string("role");
-          table.jsonb("file").nullable();
-        });
-    }
-    await this.db<Message>(`msg${roomId}`)
-      .withSchema(this.qid)
-      .insert({
-        ...message,
-        _id: `${message._id}`,
+      await this.db.schema.createTable(`msg${roomId}`, (table) => {
+        table
+          .string("_id")
+          .unique()
+          .index()
+          .primary()
+          .unsigned();
+        table.string("senderId");
+        table.string("username");
+        table.text("content").nullable();
+        table.text("code").nullable();
+        table.string("timestamp");
+        table.string("date");
+        table.string("role");
+        table.jsonb("file").nullable();
+        table.bigInteger("time");
+        table.jsonb("replyMessage").nullable();
+        table.boolean("at").nullable();
       });
+      await this.db("msgTableName").insert({ tableName: `msg${roomId}` });
+    }
+  }
+
+  async addMessage(roomId: number, message: Message): Promise<any> {
+    await this.createMsgTable(roomId);
+    await this.db<Message>(`msg${roomId}`).insert({
+      ...message,
+      _id: `${message._id}`,
+    });
   }
 
   async updateMessage(
@@ -253,7 +269,7 @@ export default class SQLStorageProvider implements StorageProvider {
     message: object
   ): Promise<any> {
     await this.db<Message>(`msg${roomId}`)
-      .withSchema(this.qid)
+
       .where("_id", "=", `${messageId}`)
       .update(message);
   }
@@ -263,28 +279,29 @@ export default class SQLStorageProvider implements StorageProvider {
     skip: number,
     limit: number
   ): Promise<Message[]> {
-    return await this.db<Message>(`msg${roomId}`)
-      .withSchema(this.qid)
+    await this.createMsgTable(roomId);
+    const messages = await this.db<Message>(`msg${roomId}`)
+
       .orderBy("time", "desc")
       .limit(limit)
-      .offset(skip - 1)
-      .select();
+      .offset(skip)
+      .select("*");
+    return messages.reverse().map((message) => this.msgConFromDB(message));
   }
 
   async getMessage(roomId: number, messageId: string): Promise<Message> {
-    return await this.db<Message>(`msg${roomId}`)
-      .withSchema(this.qid)
+    const message = await this.db<Message>(`msg${roomId}`)
+
       .where("_id", "=", messageId)
-      .select()[0];
+      .select("*")[0];
+    return this.msgConFromDB(message);
   }
 
   async addMessages(roomId: number, messages: Message[]): Promise<any> {
-    try {
-      await this.db<Message>(`msg${roomId}`)
-        .withSchema(this.qid)
-        .insert(messages);
-    } catch (e) {
-      return e;
-    }
+    await this.db<Message>(`msg${roomId}`)
+
+      .insert(Array.from(new Set(messages)))
+      .onConflict("_id")
+      .ignore();
   }
 }
