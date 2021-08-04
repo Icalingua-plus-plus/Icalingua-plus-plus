be38663c2a684da8fa2d0509bdb705035b94e90b
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2021-08-04 16:37:19 +0800

    修复：艾特所有人的消息无法被写入SQL数据库

--- electron-qq/src/types/Message.d.ts
+++ electron-qq/src/types/Message.d.ts
@@ -1,32 +1,32 @@
-import MessageMirai from './MessageMirai'
+import MessageMirai from "./MessageMirai";
 
 interface MessageFile {
-    type: string;
-    url: string;
-    size?: number;
-    name?: string;
-    fid?: string
+  type: string;
+  url: string;
+  size?: number;
+  name?: string;
+  fid?: string;
 }
 
 export default interface Message {
-    mirai?: MessageMirai
-    reveal?: boolean
-    code?: string;
-    at?: string | boolean;
-    _id: string | number;
-    senderId?: number;
-    username: string;
-    content: string;
-    timestamp?: string;
-    date?: string;
-    role?: string;
-    time?: number;
-    deleted?: boolean | Date | number;
-    file?: MessageFile;
-    replyMessage?: Message,
-    system?: boolean
+  mirai?: MessageMirai;
+  reveal?: boolean;
+  code?: string;
+  at?: boolean | "all";
+  _id: string | number;
+  senderId?: number;
+  username: string;
+  content: string;
+  timestamp?: string;
+  date?: string;
+  role?: string;
+  time?: number;
+  deleted?: boolean | Date | number;
+  file?: MessageFile;
+  replyMessage?: Message;
+  system?: boolean;
 }
 
 interface MessageInIDB extends Message {
-    roomId: number;
+  roomId: number;
 }
--- electron-qq/src/types/Room.d.ts
+++ electron-qq/src/types/Room.d.ts
@@ -1,22 +1,22 @@
-import LastMessage from './LastMessage'
+import LastMessage from "./LastMessage";
 
 export default interface Room {
-    roomId: number;
-    roomName: string;
-    avatar: string;
-    index: number;
-    unreadCount: number;
-    priority: 1 | 2 | 3 | 4 | 5;
-    utime: number;
-    users:
-        [{ _id: 1; username: '1' }, { _id: 2; username: '2' }]
-        | [
-        { _id: 1; username: '1' },
-        { _id: 2; username: '2' },
-        { _id: 3; username: '3' }
-    ];
-    at?: boolean | string | null;
-    lastMessage: LastMessage;
-    autoDownload?: boolean;
-    downloadPath?: string;
+  roomId: number;
+  roomName: string;
+  avatar: string;
+  index: number;
+  unreadCount: number;
+  priority: 1 | 2 | 3 | 4 | 5;
+  utime: number;
+  users:
+    | [{ _id: 1; username: "1" }, { _id: 2; username: "2" }]
+    | [
+        { _id: 1; username: "1" },
+        { _id: 2; username: "2" },
+        { _id: 3; username: "3" }
+      ];
+  at?: boolean | "all";
+  lastMessage: LastMessage;
+  autoDownload?: boolean;
+  downloadPath?: string;
 }
