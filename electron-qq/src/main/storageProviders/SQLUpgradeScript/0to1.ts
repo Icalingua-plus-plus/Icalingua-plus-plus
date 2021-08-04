ef7a35d2e88029a1fd91bc526a1a36c151c7d21c
Icalingua <Icalinguaplusplus@protonmail.com>
2021-08-04 23:16:35 +0800

    分离故障处理，进行故障记录

--- electron-qq/src/main/storageProviders/SQLUpgradeScript/0to1.ts
+++ electron-qq/src/main/storageProviders/SQLUpgradeScript/0to1.ts
@@ -2,33 +2,28 @@ import { Knex } from "knex";
 import { DBVersion, MsgTableName } from "../../../types/SQLTableTypes";
 
 const upg0to1 = async (db: Knex) => {
-  try {
-    await db.schema.alterTable("rooms", (table) => {
-      table.dropColumn("at");
-    });
-    await db.schema.alterTable("rooms", (table) => {
-      table.string("at");
-    });
-    const msgTableNames = await db<MsgTableName>("msgTableName").select(
-      "tableName"
-    );
-    const msgTableNamesAry = msgTableNames.map((obj) => obj.tableName);
-    if (msgTableNamesAry.length !== 0) {
-      const PAry = msgTableNamesAry.map(async (msgTableName) => {
-        await db.schema.alterTable(msgTableName, (table) => {
-          table.dropColumn("at");
-        });
-        await db.schema.alterTable(msgTableName, (table) => {
-          table.string("at");
-        });
+  await db.schema.alterTable("rooms", (table) => {
+    table.dropColumn("at");
+  });
+  await db.schema.alterTable("rooms", (table) => {
+    table.string("at");
+  });
+  const msgTableNames = await db<MsgTableName>("msgTableName").select(
+    "tableName"
+  );
+  const msgTableNamesAry = msgTableNames.map((obj) => obj.tableName);
+  if (msgTableNamesAry.length !== 0) {
+    const PAry = msgTableNamesAry.map(async (msgTableName) => {
+      await db.schema.alterTable(msgTableName, (table) => {
+        table.dropColumn("at");
       });
-      await Promise.all(PAry);
-    }
-    await db<DBVersion>("dbVersion").update({ dbVersion: 1 });
-  } catch (e) {
-    console.error(e);
-    throw e;
+      await db.schema.alterTable(msgTableName, (table) => {
+        table.string("at");
+      });
+    });
+    await Promise.all(PAry);
   }
+  await db<DBVersion>("dbVersion").update({ dbVersion: 1 });
 };
 
 export default upg0to1;
