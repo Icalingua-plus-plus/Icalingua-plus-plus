248b8b5698a52bdedff8d96eaa3168cccdc7c72a
凌莞 <i@gao4.pw>
2021-08-11 15:23:43 +0800

    同时迁移自定义字体

--- icalingua/src/main/utils/migrateData.ts
+++ icalingua/src/main/utils/migrateData.ts
@@ -9,5 +9,6 @@ const storageOld = path.join(storageNew, '../electron-qq')
 export default () => {
     fs.copyFileSync(path.join(storageOld, 'config.yaml'), path.join(storageNew, 'config.yaml'))
     childProcess.spawn('mv', [path.join(storageOld, 'stickers'), path.join(storageNew, 'stickers')])
+    childProcess.spawn('mv', [path.join(storageOld, 'font.ttf'), path.join(storageNew, 'font.ttf')])
     childProcess.spawn('sh', ['-c', `mv ${path.join(storageOld, 'eqq*.db')} ${storageNew}`])
 }
