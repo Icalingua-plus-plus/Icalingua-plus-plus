950c9beb3692ecbb5c04c2136986cc418e5ca7f2
RainbowBird <rbxin2003@outlook.com>
2021-08-11 12:28:49 +0800

    修复代码中的一些小问题
    Signed-off-by: RainbowBird <rbxin2003@outlook.com>

--- electron-qq/src/renderer/components/TheContactsPanel.vue
+++ electron-qq/src/renderer/components/TheContactsPanel.vue
@@ -1,55 +1,56 @@
 <template>
-	<div class="contacts-root">
-		<div class="contacts-head">
-			<el-input
-				v-model="searchContext"
-				placeholder="Search"
-				prefix-icon="el-icon-search"
-				clearable
-			/>
-		</div>
+  <div class="contacts-root">
+    <div class="contacts-head">
+      <el-input
+          v-model="searchContext"
+          placeholder="Search"
+          prefix-icon="el-icon-search"
+          clearable
+      />
+    </div>
 
-		<div class="contacts-content">
-			<el-tabs v-model="activeName" :stretch="true">
-				<el-tab-pane label="Friends" name="friends">
-					<el-collapse v-model="activeFriendGroup">
-						<el-collapse-item v-for="(v, i) in friendsAll"
-						                  :title="`${v.name} (${v.friends.filter(e=>e.sc.includes(searchContext)).length})`"
-						                  :name="i" :key="i">
-							<ContactEntry
-								v-for="i in v.friends"
-								:key="i.uin"
-								:id="i.uin"
-								:remark="i.remark"
-								:name="i.nick"
-								v-show="i.sc.includes(searchContext)"
-								@dblclick="$emit('dblclick', i.uin, i.remark)"
-							/>
-						</el-collapse-item>
-						<ContactEntry
-							v-for="i in friendsFallback"
-							:key="i.uin"
-							:id="i.uin"
-							:remark="i.remark"
-							:name="i.nick"
-							v-show="i.sc.includes(searchContext)"
-							@dblclick="$emit('dblclick', i.uin, i.remark)"
-						/>
-					</el-collapse>
-				</el-tab-pane>
-				<el-tab-pane label="Groups" name="groups">
-					<ContactEntry
-						v-for="i in groupsAll"
-						:key="i.group_id"
-						:id="-i.group_id"
-						:remark="i.group_name"
-						v-show="i.sc.includes(searchContext)"
-						@dblclick="$emit('dblclick', -i.group_id, i.group_name)"
-					/>
-				</el-tab-pane>
-			</el-tabs>
-		</div>
-	</div>
+    <div class="contacts-content">
+      <el-tabs v-model="activeName" :stretch="true">
+        <el-tab-pane label="Friends" name="friends">
+          <div
+              class="contacts-panel"
+              v-show="activeName === 'friends'"
+          >
+            <el-collapse v-model="activeFriendGroup">
+              <el-collapse-item v-for="(v, i) in friendsAll"
+                                :title="`${v.name} (${v.friends.filter(e=>e.sc.includes(searchContext)).length})`"
+                                :name="i" :key="i">
+                <ContactEntry
+                    v-for="i in v.friends"
+                    :key="i.uin"
+                    :id="i.uin"
+                    :remark="i.remark"
+                    :name="i.nick"
+                    v-show="i.sc.includes(searchContext)"
+                    @dblclick="$emit('dblclick', i.uin, i.remark)"
+                />
+              </el-collapse-item>
+            </el-collapse>
+          </div>
+        </el-tab-pane>
+        <el-tab-pane label="Groups" name="groups">
+          <div
+              class="contacts-panel"
+              v-show="activeName === 'groups'"
+          >
+            <ContactEntry
+                v-for="i in groupsAll"
+                :key="i.group_id"
+                :id="-i.group_id"
+                :remark="i.group_name"
+                v-show="i.sc.includes(searchContext)"
+                @dblclick="$emit('dblclick', -i.group_id, i.group_name)"
+            />
+          </div>
+        </el-tab-pane>
+      </el-tabs>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -57,66 +58,64 @@ import {ipcRenderer} from 'electron'
 import ContactEntry from './ContactEntry.vue'
 
 export default {
-	components: {ContactEntry},
-	data() {
-		return {
-			activeName: 'friends',
-			groupsAll: [],
-			/**
-			 * @type GroupOfFriend[]
-			 */
-			friendsAll: [],
-			searchContextEdit: '',
-			activeFriendGroup: [],
-			friendsFallback: [],
-		}
-	},
-	computed: {
-		searchContext: {
-			get() {
-				return this.searchContextEdit
-			},
-			set(val) {
-				this.searchContextEdit = val.toUpperCase()
-			},
-		},
-	},
-	created() {
-		ipcRenderer.invoke('getFriendsAndGroups')
-			.then(({friends, groups, friendsFallback}) => {
-				this.friendsAll = friends ? Object.freeze(friends) : null
-				this.groupsAll = Object.freeze(groups)
-				friendsFallback && (this.friendsFallback = Object.freeze(friendsFallback))
-			})
-	},
+  components: {ContactEntry},
+  data() {
+    return {
+      activeName: 'friends',
+      groupsAll: [],
+      /**
+       * @type GroupOfFriend[]
+       */
+      friendsAll: [],
+      searchContextEdit: '',
+      activeFriendGroup: [],
+    }
+  },
+  computed: {
+    searchContext: {
+      get() {
+        return this.searchContextEdit
+      },
+      set(val) {
+        this.searchContextEdit = val.toUpperCase()
+      },
+    },
+  },
+  created() {
+    ipcRenderer.invoke('getFriendsAndGroups')
+        .then(({friends, groups}) => {
+          this.friendsAll = Object.freeze(friends)
+          this.groupsAll = Object.freeze(groups)
+        })
+  },
 }
 </script>
 
 <style>
-.el-collapse-item__header {
-	padding-left: 12px;
-}
-
-.el-tabs__item {
-	padding: 0;
+.contacts-panel {
+  margin: 0 12px;
 }
 
 .contacts-root {
-	border-right: 1px solid #e1e4e8;
-	height: 100vh;
-	display: flex;
-	flex-direction: column;
+  border-right: 1px solid #e1e4e8;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
 }
 
 .contacts-head {
-	margin: 0 12px;
-	height: 64px;
-	min-height: 64px;
-	display: flex;
-	align-items: center;
+  margin: 0 12px;
+  height: 64px;
+  min-height: 64px;
+  display: flex;
+  align-items: center;
 }
 
 .contacts-content {
-	overflow: auto;
+  overflow: auto;
+}
+
+.el-tabs__header {
+  margin: unset !important;
 }
 </style>
