402a56bb7720e958bb4f76a1a06ff8cd4f483c85
Icalingua <Icalinguaplusplus@protonmail.com>
2021-08-20 14:31:01 +0800

    fix #173

--- icalingua/src/main/utils/migrateData.ts
+++ icalingua/src/main/utils/migrateData.ts
@@ -8,6 +8,7 @@ const storageOld = path.join(storageNew, '../electron-qq')
 
 export default () => {
     fs.copyFileSync(path.join(storageOld, 'config.yaml'), path.join(storageNew, 'config.yaml'))
+    childProcess.spawn('mv', [path.join(storageOld, 'data'), path.join(storageNew, 'data')])
     childProcess.spawn('mv', [path.join(storageOld, 'stickers'), path.join(storageNew, 'stickers')])
     childProcess.spawn('mv', [path.join(storageOld, 'databases'), path.join(storageNew, 'databases')])
     childProcess.spawn('mv', [path.join(storageOld, 'font.ttf'), path.join(storageNew, 'font.ttf')])
