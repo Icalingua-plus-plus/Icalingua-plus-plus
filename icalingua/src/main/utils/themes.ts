3fcee52b09b077ae004e898fef05085e52c8ba7b
Icalingua <Icalinguaplusplus@protonmail.com>
2021-08-25 12:04:14 +0800

    Theme selector

---
+++ icalingua/src/main/utils/themes.ts
@@ -0,0 +1,24 @@
+import * as windowMgr from './windowManager';
+import * as menuMgr from '../ipc/menuManager';
+import { ipcMain } from 'electron';
+import * as configMgr from './configManager';
+
+
+var themeList = ['light', 'dark'];
+
+export function getThemeList() { return themeList; }
+
+export function refreshTheme() {
+    windowMgr.sendToMainWindow('theme:refresh')
+}
+
+export function useTheme(theme: string) {
+    windowMgr.sendToMainWindow('theme:use', theme);
+}
+
+ipcMain.on('theme:list-complete', (_, list) => {
+    themeList = list;
+    menuMgr.updateAppMenu();
+    let theme = configMgr.getConfig().theme;
+    if (theme != undefined) useTheme(theme);
+});
