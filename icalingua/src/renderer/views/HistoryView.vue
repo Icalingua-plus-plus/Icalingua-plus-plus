97e0fc50f3b66b05dbb910264e126f897e451708
Icalingua <Icalinguaplusplus@protonmail.com>
2021-09-01 17:11:34 +0800

    fix #203

--- icalingua/src/renderer/views/HistoryView.vue
+++ icalingua/src/renderer/views/HistoryView.vue
@@ -1,36 +1,36 @@
 <template>
-	<Room
-		class="vac-card-window icalingua-theme-holder"
-		:current-user-id="0"
-		:rooms="[room]"
-		:messages="messages"
-		height="100vh"
-		:rooms-loaded="true"
-		:messages-loaded="true"
-		:show-audio="false"
-		:show-reaction-emojis="false"
-		:show-new-messages-divider="false"
-		:load-first-room="true"
-		accepted-files="image/*"
-		:message-actions="[]"
-		:styles="styles"
-		:single-room="true"
-		:room-id="0"
-		:show-footer="false"
-		:show-header="false"
-		:show-rooms-list="false"
-		:is-mobile="false"
-		:menu-actions="[]"
-		:show-send-icon="true"
-		:show-files="true"
-		:show-emojis="true"
-		:loading-rooms="false"
-		:text-formatting="true"
-		:style="[cssVars]"
-		@download-image="downloadImage"
-		@open-file="openImage"
-		@open-forward="openForward"
-	/>
+    <Room
+        class="vac-card-window icalingua-theme-holder"
+        :current-user-id="0"
+        :rooms="[room]"
+        :messages="messages"
+        height="100vh"
+        :rooms-loaded="true"
+        :messages-loaded="true"
+        :show-audio="false"
+        :show-reaction-emojis="false"
+        :show-new-messages-divider="false"
+        :load-first-room="true"
+        accepted-files="image/*"
+        :message-actions="[]"
+        :styles="styles"
+        :single-room="true"
+        :room-id="0"
+        :show-footer="false"
+        :show-header="false"
+        :show-rooms-list="false"
+        :is-mobile="false"
+        :menu-actions="[]"
+        :show-send-icon="true"
+        :show-files="true"
+        :show-emojis="true"
+        :loading-rooms="false"
+        :text-formatting="true"
+        :style="[cssVars]"
+        @download-image="downloadImage"
+        @open-file="openImage"
+        @open-forward="openForward"
+    />
 </template>
 
 <script>
@@ -40,53 +40,53 @@ import ipc from '../utils/ipc'
 import * as themes from '../utils/themes'
 
 export default {
-	name: 'HistoryView',
-	data() {
-		return {
-			room: {
-				roomId: 0,
-				roomName: 'Forwarded Messages',
-				users: [
-					{_id: 3, username: '3'},
-					{_id: 31, username: '3'},
-					{_id: 32, username: '3'},
-				],
-			},
-			messages: [],
-		}
-	},
-	created() {
-		document.title = '查看转发的消息记录'
-		ipcRenderer.on('loadMessages', (event, args) => {
-			console.log(args)
-			this.messages = [...args]
-		})
-		document.addEventListener('keydown', (e) => {
-			if (e.repeat) return
-			if (e.key === 'w' && e.ctrlKey === true) {
-				window.close()
-			}
-		})
-	},
-	components: {
-		Room,
-	},
-	computed: {
-		cssVars() {
-			return themes.recalcTheme();
-		},
-	},
-	methods: {
-		openForward: ipc.openForward,
-		openImage: ipc.downloadFileByMessageData,
-		downloadImage: ipc.downloadImage,
-	},
+    name: 'HistoryView',
+    data() {
+        return {
+            room: {
+                roomId: 0,
+                roomName: 'Forwarded Messages',
+                users: [
+                    {_id: 3, username: '3'},
+                    {_id: 31, username: '3'},
+                    {_id: 32, username: '3'},
+                ],
+            },
+            messages: [],
+        }
+    },
+    created() {
+        document.title = '查看转发的消息记录'
+        ipcRenderer.on('loadMessages', (event, args) => {
+            console.log(args)
+            this.messages = [...args]
+        })
+        document.addEventListener('keydown', (e) => {
+            if (e.repeat) return
+            if (e.key === 'w' && e.ctrlKey === true) {
+                window.close()
+            }
+        })
+    },
+    components: {
+        Room,
+    },
+    computed: {
+        cssVars() {
+            return themes.recalcTheme()
+        },
+    },
+    methods: {
+        openForward: ipc.openForward,
+        openImage: ipc.downloadFileByMessageData,
+        downloadImage: ipc.downloadImage,
+    },
 }
 </script>
 
 <style scoped>
 ::v-deep .vac-col-messages {
-	height: 100vh;
+  height: 100vh;
 }
 </style>
 
@@ -94,43 +94,40 @@ export default {
 @import "../components/vac-mod/styles/index.scss";
 
 .vac-card-window {
-	display: block;
-	background: var(--chat-content-bg-color);
-	color: var(--chat-color);
-	overflow-wrap: break-word;
-	position: relative;
-	white-space: normal;
-	border: var(--chat-container-border);
-	border-radius: var(--chat-container-border-radius);
-	box-shadow: var(--chat-container-box-shadow);
-	-webkit-tap-highlight-color: transparent;
+  min-height: 100vh;
+  display: block;
+  background: var(--chat-content-bg-color);
+  color: var(--chat-color);
+  overflow-wrap: break-word;
+  position: relative;
+  white-space: normal;
+  border: var(--chat-container-border);
+  border-radius: var(--chat-container-border-radius);
+  box-shadow: var(--chat-container-box-shadow);
+  -webkit-tap-highlight-color: transparent;
 
-	* {
-		font-family: inherit;
-	}
+  * {
+    font-family: inherit;
+  }
 
-	a {
-		color: #0d579c;
-		font-weight: 500;
-	}
+  a {
+    color: #0d579c;
+    font-weight: 500;
+  }
 
-	.vac-chat-container {
-		height: 100%;
-		display: flex;
+  .vac-chat-container {
+    height: 100%;
+    display: flex;
 
-		input {
-			min-width: 10px;
-		}
+    input {
+      min-width: 10px;
+    }
 
-		textarea,
-		input[type="text"],
-		input[type="search"] {
-			-webkit-appearance: none;
-		}
-	}
-}
-
-html, body, #app, .vac-card-window {
-	min-height: 100vh;
+    textarea,
+    input[type="text"],
+    input[type="search"] {
+      -webkit-appearance: none;
+    }
+  }
 }
 </style>
