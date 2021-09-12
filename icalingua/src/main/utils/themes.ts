c6779ab14a24c8416bf9bf4e1cc3ae095f69e51e
Icalingua <Icalinguaplusplus@protonmail.com>
2021-09-12 11:22:06 +0800

    跟随系统暗色模式 close #211

--- icalingua/src/main/utils/themes.ts
+++ icalingua/src/main/utils/themes.ts
@@ -1,31 +1,45 @@
-import * as windowMgr from './windowManager';
-import * as menuMgr from '../ipc/menuManager';
-import { ipcMain } from 'electron';
-import * as configMgr from './configManager';
+import * as windowMgr from './windowManager'
+import * as menuMgr from '../ipc/menuManager'
+import {ipcMain, nativeTheme} from 'electron'
+import * as configMgr from './configManager'
 
 
-var themeList = ['light', 'dark'];
-var themeData: any = {};
+var themeList = ['light', 'dark']
+var themeData: any = {}
 
-export function getThemeData() { return themeData; }
+export function getThemeData() {
+    return themeData
+}
 
-export function getThemeList() { return themeList; }
+export function getThemeList() {
+    return themeList
+}
 
 export function refreshTheme() {
     windowMgr.sendToMainWindow('theme:refresh')
 }
 
 export function useTheme(theme: string) {
-    windowMgr.sendToMainWindow('theme:use', theme);
+    windowMgr.sendToMainWindow('theme:use', theme)
 }
 
 ipcMain.on('theme:list-complete', (_, list) => {
-    themeList = list;
-    menuMgr.updateAppMenu();
-    let theme = configMgr.getConfig().theme;
-    if (theme != undefined) useTheme(theme);
-});
+    themeList = list
+    menuMgr.updateAppMenu()
+    let theme = configMgr.getConfig().theme
+    if (theme != undefined)
+        theme === 'auto' ? autoSetTheme() : useTheme(theme)
+})
 
 ipcMain.on('theme:theme-data', (_, data) => {
-    themeData = data;
-});
+    themeData = data
+})
+
+export const autoSetTheme = () => {
+    useTheme(nativeTheme.shouldUseDarkColors ? 'dark' : 'light')
+}
+
+nativeTheme.on('updated', e => {
+    if (configMgr.getConfig().theme === 'auto')
+        autoSetTheme()
+})
