dfffaa3e9fb5bbf3237502af61421ba5498b9e29
Icalingua <Icalinguaplusplus@protonmail.com>
2021-10-01 13:00:12 +0800

    修复使用腾讯云传递头像时查看头像还是转发 bot 的头像

--- icalingua/src/main/ipc/menuManager.ts
+++ icalingua/src/main/ipc/menuManager.ts
@@ -885,6 +885,11 @@ ipcMain.on('popupAvatarMenu', async (e, message: Message, room: Room) => {
             if (message.mirai && message.mirai.eqq.avatarMd5) {
                 openImage(getImageUrlByMd5(message.mirai.eqq.avatarMd5))
             }
+            else if (message.mirai && message.mirai.eqq.avatarUrl) {
+                const QCLOUD_AVATAR_REGEX = /^https:\/\/[a-z0-9\-]+\.cos\.[a-z\-]+\.myqcloud\.com\/[0-9]+-[0-9]+\.jpg$/
+                if (QCLOUD_AVATAR_REGEX.test(message.mirai.eqq.avatarUrl))
+                    openImage(message.mirai.eqq.avatarUrl)
+            }
             else {
                 openImage(getAvatarUrl(message.senderId))
             }
