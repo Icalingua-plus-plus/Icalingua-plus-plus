1280507b3d7a3e3cfc000d8bb2792d7a970bc90b
Icalingua <Icalinguaplusplus@protonmail.com>
2021-10-02 22:36:17 +0800

    `Message` 增加 `files` 字段

--- icalingua/src/types/Message.d.ts
+++ icalingua/src/types/Message.d.ts
@@ -1,34 +1,35 @@
-import MessageMirai from "./MessageMirai";
+import MessageMirai from './MessageMirai'
 
 interface MessageFile {
-  type: string;
-  url: string;
-  size?: number;
-  name?: string;
-  fid?: string;
+    type: string;
+    url: string;
+    size?: number;
+    name?: string;
+    fid?: string;
 }
 
 export default interface Message {
-  _id: string | number;
-  senderId?: number;
-  username: string;
-  content: string;
-  code?: string;
-  timestamp?: string;
-  date?: string;
-  role?: string;
-  file?: MessageFile;
-  time?: number;
-  replyMessage?: Message;
-  at?: boolean | "all";
-  deleted?: boolean;
-  system?: boolean;
-  mirai?: MessageMirai;
-  reveal?: boolean;
-  flash?: boolean;
-  title?: string;
+    _id: string | number;
+    senderId?: number;
+    username: string;
+    content: string;
+    code?: string;
+    timestamp?: string;
+    date?: string;
+    role?: string;
+    file?: MessageFile;
+    files?: MessageFile[];
+    time?: number;
+    replyMessage?: Message;
+    at?: boolean | 'all';
+    deleted?: boolean;
+    system?: boolean;
+    mirai?: MessageMirai;
+    reveal?: boolean;
+    flash?: boolean;
+    title?: string;
 }
 
 interface MessageInIDB extends Message {
-  roomId: number;
+    roomId: number;
 }
