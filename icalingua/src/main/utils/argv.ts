bb3ea9cfad1a0840acaa72d374bcd03faf339703
RainbowBird <rbxin2003@outlook.com>
2021-10-03 22:12:28 +0800

    支持命令行窗口隐藏
    Signed-off-by: RainbowBird <rbxin2003@outlook.com>

--- icalingua/src/main/utils/argv.ts
+++ icalingua/src/main/utils/argv.ts
@@ -3,13 +3,15 @@ import minimist from 'minimist'
 type Args = {
     config?: string
     version?: boolean
+    hide?: boolean
 }
 
 export default minimist(process.argv, {
     string: ['config'],
-    boolean: ['version'],
+    boolean: ['version', 'hide'],
     alias: {
         c: 'config',
         V: 'version',
+        h: 'hide',
     },
 }) as Args
