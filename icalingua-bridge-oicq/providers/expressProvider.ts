409dd8c9f01343df28151a9f8251a3c5733d412d
Icalingua <Icalinguaplusplus@protonmail.com>
2021-10-08 23:16:25 +0800

    更改名称

--- icalingua-bridge-oicq/providers/expressProvider.ts
+++ icalingua-bridge-oicq/providers/expressProvider.ts
@@ -1,15 +1,15 @@
 import express from 'express'
-import tokenManager from '../utils/tokenManager'
+import SendImgTokenManager from '../utils/SendImgTokenManager'
 import adapter from '../adapters/oicqAdapter'
 import {json} from 'body-parser'
 
 export const app = express()
 const parser = json({
-    limit: '5mb'
+    limit: '10mb'
 })
 
 app.post('/api/:token/sendMessage', parser, (req, res) => {
-    if (req.params.token && tokenManager.verify(req.params.token)) {
+    if (req.params.token && SendImgTokenManager.verify(req.params.token)) {
         adapter.sendMessage(req.body)
         res.sendStatus(202).end()
     } else
