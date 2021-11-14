6ae7342c70988d5f4481c8d27f96ac8833c7cd40
Icalingua <Icalinguaplusplus@protonmail.com>
2021-11-14 10:02:13 +0800

    试图修正 SQLite 并发性能不足导致的问题

--- icalingua/src/storageProviders/SQLUpgradeScript/6to7.ts
+++ icalingua/src/storageProviders/SQLUpgradeScript/6to7.ts
@@ -6,7 +6,7 @@ import {
   MsgTableName,
 } from "../../types/SQLTableTypes";
 
-const upg6to7 = async (db: Knex) => {
+const upg6to7 = async (db: Knex, dbType: "pg" | "mysql" | "sqlite3") => {
   console.log(
     "Applying SQL Storage Provider Update:\n",
     "Version 7\n",
@@ -17,29 +17,57 @@ const upg6to7 = async (db: Knex) => {
   );
   const msgTableNamesAry = msgTableNames.map((obj) => obj.tableName);
   if (msgTableNamesAry.length !== 0) {
-    const PAry = msgTableNamesAry.map(async (msgTableName) => {
-      return new Promise<void>((resolve) => {
-        const roomId = msgTableName.substring(3);
-        const messageStream = db<Message>(msgTableName)
-          .select("*")
-          .stream();
-        messageStream.on("data", async (data: Message) => {
-          try {
-            await db<MessageInSQLDB>("messages").insert({
-              ...data,
-              roomId: Number(roomId),
-            });
-          } catch (e) {
-            console.error(e);
-          }
-        });
-        messageStream.on("end", async () => {
-          await db.schema.dropTable(msgTableName);
-          resolve();
+    // SQLite3 似乎受不起这样的折腾
+    if (dbType !== "sqlite3") {
+      const PAry = msgTableNamesAry.map(async (msgTableName) => {
+        return new Promise<void>((resolve) => {
+          const roomId = msgTableName.substring(3);
+          const messageStream = db<Message>(msgTableName)
+            .select("*")
+            .stream();
+          messageStream.on("data", async (data: Message) => {
+            try {
+              await db<MessageInSQLDB>("messages")
+                .insert({
+                  ...data,
+                  roomId: Number(roomId),
+                })
+                .onConflict("_id")
+                .ignore();
+            } catch (e) {
+              console.error(e);
+            }
+          });
+          messageStream.on("end", async () => {
+            await db.schema.dropTable(msgTableName);
+            resolve();
+          });
         });
       });
-    });
-    await Promise.all(PAry);
+      await Promise.all(PAry);
+    } else {
+      for await (const msgTableName of msgTableNamesAry) {
+        const roomId = msgTableName.substring(3);
+        const msgCount = await db<Message>(msgTableName).count("_id");
+        const msgCountNum = Number(msgCount[0].count);
+        for (let i = 0; i <= msgCountNum; i += 200) {
+          const messages = await db<Message>(msgTableName)
+            .select("*")
+            .offset(i)
+            .limit(200);
+          await db<MessageInSQLDB>("messages")
+            .insert(
+              messages.map((data) => ({
+                ...data,
+                roomId: Number(roomId),
+              }))
+            )
+            .onConflict("_id")
+            .ignore();
+        }
+        await db.schema.dropTable(msgTableName);
+      }
+    }
   }
   await db.schema.dropTable("msgTableName");
   await db<DBVersion>("dbVersion").update({ dbVersion: 7 });
