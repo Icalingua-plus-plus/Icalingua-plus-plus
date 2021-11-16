5101f3ca696020e16bbf9eda33640f2d0207ceaf
凌莞 <i@gao4.pw>
2021-11-16 18:52:11 +0800

    防止头像缓存

--- icalingua/src/renderer/components/GroupEntry.vue
+++ icalingua/src/renderer/components/GroupEntry.vue
@@ -1,28 +1,29 @@
 <template>
-  <a @click="$emit('click')">
-    <el-card class="card">
-      <div class="left">
-        <el-avatar
-          size="large"
-          :src="
-            chat.id < 0
-              ? 'https://p.qlogo.cn/gh/' + -chat.id + '/' + -chat.id + '/0'
-              : `https://q1.qlogo.cn/g?b=qq&nk=${chat.id}&s=640`
-          "
-        ></el-avatar>
-      </div>
-      <div class="right">
-        <p class="name withoutdesc">{{ chat.name }}</p>
-      </div>
-    </el-card>
-  </a>
+    <a @click="$emit('click')">
+        <el-card class="card">
+            <div class="left">
+                <el-avatar
+                    size="large"
+                    :src="getAvatarUrl(chat.id)"
+                />
+            </div>
+            <div class="right">
+                <p class="name withoutdesc">{{ chat.name }}</p>
+            </div>
+        </el-card>
+    </a>
 </template>
 
 <script>
+import getAvatarUrl from '../../utils/getAvatarUrl'
+
 export default {
-  name: "GroupEntry",
-  props: ["chat"],
-};
+    name: 'GroupEntry',
+    props: ['chat'],
+    methods: {
+        getAvatarUrl,
+    },
+}
 </script>
 
 <style scoped>
@@ -30,24 +31,30 @@ export default {
   margin-bottom: 10px;
   cursor: pointer;
 }
+
 .left {
   width: max-content;
   float: left;
 }
+
 .right {
   margin-left: 50px;
 }
+
 a {
   text-decoration: none;
 }
+
 .desc {
   margin-top: 10px;
   color: #606266;
 }
+
 .name {
   font-weight: bold;
   color: #303133;
 }
+
 .withoutdesc {
   margin-top: 10px;
   margin-bottom: 10px;
--- icalingua/src/utils/getAvatarUrl.ts
+++ icalingua/src/utils/getAvatarUrl.ts
@@ -1,5 +1,11 @@
+let timestamp = new Date().getTime()
+
+setInterval(() => {
+    timestamp = new Date().getTime()
+}, 1000 * 60)
+
 export default (roomId: number): string => {
     return (roomId < 0)
-        ? `https://p.qlogo.cn/gh/${-roomId}/${-roomId}/0`
-        : `https://q1.qlogo.cn/g?b=qq&nk=${roomId}&s=640`
+        ? `https://p.qlogo.cn/gh/${-roomId}/${-roomId}/0?timestamp=${timestamp}`
+        : `https://q1.qlogo.cn/g?b=qq&nk=${roomId}&s=640&timestamp=${timestamp}`
 }
