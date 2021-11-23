49c18997994e7c72f8434841d5acfc9d68f716f9
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2021-11-23 18:20:30 +0800

    考虑各数据库并发性能不一，取消并发升级

--- icalingua/src/storageProviders/SQLUpgradeScript/6to7.ts
+++ icalingua/src/storageProviders/SQLUpgradeScript/6to7.ts
@@ -17,67 +17,32 @@ const upg6to7 = async (db: Knex, dbType: "pg" | "mysql" | "sqlite3") => {
   );
   const msgTableNamesAry = msgTableNames.map((obj) => obj.tableName);
   if (msgTableNamesAry.length !== 0) {
-    // SQLite3 似乎受不起这样的折腾
-    if (dbType !== "sqlite3") {
-      const PAry = msgTableNamesAry.map(async (msgTableName) => {
-        const hasMsgTable = await db.schema.hasTable(msgTableName);
-        if (!hasMsgTable) {
-          return Promise.resolve();
-        } else {
-          return new Promise<void>((resolve) => {
-            const roomId = msgTableName.substring(3);
-            const messageStream = db<Message>(msgTableName)
+    for await (const msgTableName of msgTableNamesAry) {
+      const hasMsgTable = await db.schema.hasTable(msgTableName);
+      if (hasMsgTable) {
+        const roomId = msgTableName.substring(3);
+        const msgCount = await db<Message>(msgTableName).count("_id");
+        const msgCountNum = Number(msgCount[0].count);
+        for (let i = 0; i <= msgCountNum; i += 200) {
+          try {
+            const messages = await db<Message>(msgTableName)
               .select("*")
-              .stream();
-            messageStream.on("data", async (data: Message) => {
-              try {
-                await db<MessageInSQLDB>("messages")
-                  .insert({
-                    ...data,
-                    roomId: Number(roomId),
-                  })
-                  .onConflict("_id")
-                  .ignore();
-              } catch (e) {
-                console.error(e);
-              }
-            });
-            messageStream.on("end", async () => {
-              await db.schema.dropTable(msgTableName);
-              resolve();
-            });
-          });
-        }
-      });
-      await Promise.all(PAry);
-    } else {
-      for await (const msgTableName of msgTableNamesAry) {
-        const hasMsgTable = await db.schema.hasTable(msgTableName);
-        if (hasMsgTable) {
-          const roomId = msgTableName.substring(3);
-          const msgCount = await db<Message>(msgTableName).count("_id");
-          const msgCountNum = Number(msgCount[0].count);
-          for (let i = 0; i <= msgCountNum; i += 200) {
-            try {
-              const messages = await db<Message>(msgTableName)
-                .select("*")
-                .offset(i)
-                .limit(200);
-              await db<MessageInSQLDB>("messages")
-                .insert(
-                  messages.map((data) => ({
-                    ...data,
-                    roomId: Number(roomId),
-                  }))
-                )
-                .onConflict("_id")
-                .ignore();
-            } catch (e) {
-              console.error(e);
-            }
+              .offset(i)
+              .limit(200);
+            await db<MessageInSQLDB>("messages")
+              .insert(
+                messages.map((data) => ({
+                  ...data,
+                  roomId: Number(roomId),
+                }))
+              )
+              .onConflict("_id")
+              .ignore();
+          } catch (e) {
+            console.error(e);
           }
-          await db.schema.dropTable(msgTableName);
         }
+        await db.schema.dropTable(msgTableName);
       }
     }
   }
