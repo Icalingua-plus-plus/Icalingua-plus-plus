9d794a3144864e54990b9c5e6476d565ab897903
凌莞 <i@gao4.pw>
2021-11-25 21:20:09 +0800

    时间戳不更新了

--- icalingua/src/utils/getAvatarUrl.ts
+++ icalingua/src/utils/getAvatarUrl.ts
@@ -1,9 +1,5 @@
 let timestamp = new Date().getTime()
 
-setInterval(() => {
-    timestamp = new Date().getTime()
-}, 1000 * 60)
-
 export default (roomId: number, cache = false): string => {
     return (roomId < 0)
         ? `https://p.qlogo.cn/gh/${-roomId}/${-roomId}/0` + (cache ? '' : `?timestamp=${timestamp}`)
