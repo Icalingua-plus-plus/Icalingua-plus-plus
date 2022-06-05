85b43bd8eeb95469cb79f30accaee6bbdd406def
Icalinguaplusplus <Icalinguaplusplus@protonmail.com>
2022-06-05 14:31:47 +0800

    fix: window size config

--- icalingua/src/main/utils/exit.ts
+++ icalingua/src/main/utils/exit.ts
@@ -4,9 +4,10 @@ import { getConfig, saveConfigFile } from './configManager'
 const exit = () => {
     const win = getMainWindow()
     const size = win.getSize()
+    const originSize = getConfig().winSize
     getConfig().winSize = {
-        width: size[0],
-        height: size[1],
+        width: win.isMaximized() ? originSize.width : size[0],
+        height: win.isMaximized() ? originSize.height : size[1],
         max: win.isMaximized(),
     }
     saveConfigFile()
