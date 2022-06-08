b587bf6ac3fb1fce69a2eb482070ff7dfded17a5
Icalinguaplusplus <Icalinguaplusplus@protonmail.com>
2022-06-08 15:07:42 +0800

    chore: add Windows on ARM release build

--- .github/winrename.bat
+++ .github/winrename.bat
@@ -1,6 +1,7 @@
 @echo off
 set "str1= Setup "
 set "str2=-"
+set "str3=.exe"
 Setlocal Enabledelayedexpansion
 for /f "delims=" %%i in ('dir /b *.*') do (
-set "var=%%i" & ren "%%i" "!var:%str1%=%str2%!")
+set "var=%%i" & set "var2=!var:%str3%=_%1%str3%!" & ren "%%i" "!var2:%str1%=%str2%!")
