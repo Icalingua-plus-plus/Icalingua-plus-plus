fccfcd688782786cefed69ba5ba93810de054cf5
Icalinguaplusplus <Icalinguaplusplus@protonmail.com>
2022-06-13 23:46:23 +0800

    feat: 插件增加 makeForwardMsg

--- icalingua/src/main/adapters/oicqAdapter.ts
+++ icalingua/src/main/adapters/oicqAdapter.ts
@@ -121,6 +121,9 @@ const eventHandlers = {
                 return await bot.sendGroupMsg(group_id, message, auto_escape)
             }
             custom_bot.sendPrivateMsg = sendPrivateMsg
+            custom_bot.makeForwardMsg = async (fake, dm, target) => {
+                return await bot.makeForwardMsg(fake, dm, target);
+            }
             const custom_path = path.join(app.getPath('userData'), 'custom')
             const requireFunc = eval('require')
             requireFunc(custom_path).onMessage(data, custom_bot)
