aff2ed63aa813287ef94099be8f4e6702209624c
Icalinguaplusplus <Icalinguaplusplus@protonmail.com>
2022-06-19 01:29:30 +0800

    refactor & fix: get all windows by electron api, shortcut on macOS

--- icalingua/src/main/utils/exit.ts
+++ icalingua/src/main/utils/exit.ts
@@ -1,11 +1,12 @@
 import { getMainWindow } from './windowManager'
 import { getConfig, saveConfigFile } from './configManager'
-import { allWindows } from '../../utils/IcalinguaWindow'
+import { BrowserWindow } from 'electron'
 
 const exit = () => {
     const win = getMainWindow()
     const size = win.getSize()
     const originSize = getConfig().winSize
+    const allWindows = BrowserWindow.getAllWindows()
     getConfig().winSize = {
         width: win.isMaximized() ? originSize.width : size[0],
         height: win.isMaximized() ? originSize.height : size[1],
