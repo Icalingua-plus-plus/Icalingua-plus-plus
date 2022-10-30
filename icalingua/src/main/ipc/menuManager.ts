284a4d657dfee5c87ac5564b9f276ab3c254dfa2
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2022-10-30 13:29:18 +0800

    Fix: cannot confirm announcement

--- icalingua/src/main/ipc/menuManager.ts
+++ icalingua/src/main/ipc/menuManager.ts
@@ -226,6 +226,7 @@ const buildRoomMenu = (room: Room): Menu => {
                     for (const i in cookies) {
                         await win.webContents.session.cookies.set({
                             url: 'https://web.qun.qq.com',
+                            domain: '.qun.qq.com',
                             name: i,
                             value: cookies[i],
                         })
@@ -391,12 +392,12 @@ const buildRoomMenu = (room: Room): Menu => {
                     })
                     await win.loadURL(
                         getWinUrl() +
-                            '#/groupNickEdit/' +
-                            -room.roomId +
-                            '/' +
-                            querystring.escape(room.roomName) +
-                            '/' +
-                            querystring.escape(memberInfo.card || memberInfo.nickname),
+                        '#/groupNickEdit/' +
+                        -room.roomId +
+                        '/' +
+                        querystring.escape(room.roomName) +
+                        '/' +
+                        querystring.escape(memberInfo.card || memberInfo.nickname),
                     )
                 },
             }),
@@ -453,7 +454,7 @@ const buildRoomMenu = (room: Room): Menu => {
                     win.webContents.on('dom-ready', () =>
                         win.webContents.insertCSS(
                             '.header,.footer>p:not(:last-child),#changeGroup{display:none} ' +
-                                '.body{padding-top:0 !important;margin:0 !important}',
+                            '.body{padding-top:0 !important;margin:0 !important}',
                         ),
                     )
                     await win.loadURL('https://qun.qq.com/member.html#gid=' + -room.roomId)
@@ -971,7 +972,7 @@ export const updateAppMenu = async () => {
                                 ui.chroom(0)
                                 ui.message(
                                     '不建议关闭性能优化，关闭后长时间挂机或浏览历史记录极易导致前端卡死。' +
-                                        '关闭后若前端卡死，可尝试杀死渲染进程并重新加载，亦可直接重启。',
+                                    '关闭后若前端卡死，可尝试杀死渲染进程并重新加载，亦可直接重启。',
                                 )
                                 getConfig().optimizeMethod = 'none'
                                 ui.setOptimizeMethodSetting('none')
@@ -1674,16 +1675,16 @@ ipcMain.on('popupAvatarMenu', async (e, message: Message, room: Room) => {
                     })
                     await win.loadURL(
                         getWinUrl() +
-                            '#/MuteUser/' +
-                            -room.roomId +
-                            '/' +
-                            message.senderId +
-                            '/' +
-                            querystring.escape(room.roomName) +
-                            '/' +
-                            querystring.escape(message.username) +
-                            '/' +
-                            querystring.escape(message.anonymousflag),
+                        '#/MuteUser/' +
+                        -room.roomId +
+                        '/' +
+                        message.senderId +
+                        '/' +
+                        querystring.escape(room.roomName) +
+                        '/' +
+                        querystring.escape(message.username) +
+                        '/' +
+                        querystring.escape(message.anonymousflag),
                     )
                 },
             }),
@@ -1707,14 +1708,14 @@ ipcMain.on('popupAvatarMenu', async (e, message: Message, room: Room) => {
                     })
                     await win.loadURL(
                         getWinUrl() +
-                            '#/kickAndExit/kick/' +
-                            -room.roomId +
-                            '/' +
-                            message.senderId +
-                            '/' +
-                            querystring.escape(room.roomName) +
-                            '/' +
-                            querystring.escape(message.username),
+                        '#/kickAndExit/kick/' +
+                        -room.roomId +
+                        '/' +
+                        message.senderId +
+                        '/' +
+                        querystring.escape(room.roomName) +
+                        '/' +
+                        querystring.escape(message.username),
                     )
                 },
             }),
@@ -1773,13 +1774,13 @@ ipcMain.on('popupContactMenu', (_, remark?: string, name?: string, displayId?: n
                     })
                     await win.loadURL(
                         getWinUrl() +
-                            '#/kickAndExit/' +
-                            (group.owner_id === getUin() ? 'dismiss' : 'exit') +
-                            '/' +
-                            displayId +
-                            '/0/' +
-                            querystring.escape(remark) +
-                            '/0',
+                        '#/kickAndExit/' +
+                        (group.owner_id === getUin() ? 'dismiss' : 'exit') +
+                        '/' +
+                        displayId +
+                        '/0/' +
+                        querystring.escape(remark) +
+                        '/0',
                     )
                 },
             }),
@@ -1887,16 +1888,16 @@ ipcMain.on(
                         })
                         await win.loadURL(
                             getWinUrl() +
-                                '#/MuteUser/' +
-                                group +
-                                '/' +
-                                displayId +
-                                '/' +
-                                querystring.escape(selectedRoom.roomName) +
-                                '/' +
-                                querystring.escape(remark) +
-                                '/' +
-                                'null',
+                            '#/MuteUser/' +
+                            group +
+                            '/' +
+                            displayId +
+                            '/' +
+                            querystring.escape(selectedRoom.roomName) +
+                            '/' +
+                            querystring.escape(remark) +
+                            '/' +
+                            'null',
                         )
                     },
                 }),
@@ -1921,14 +1922,14 @@ ipcMain.on(
                         })
                         await win.loadURL(
                             getWinUrl() +
-                                '#/kickAndExit/kick/' +
-                                group +
-                                '/' +
-                                displayId +
-                                '/' +
-                                querystring.escape(selectedRoom.roomName) +
-                                '/' +
-                                querystring.escape(remark),
+                            '#/kickAndExit/kick/' +
+                            group +
+                            '/' +
+                            displayId +
+                            '/' +
+                            querystring.escape(selectedRoom.roomName) +
+                            '/' +
+                            querystring.escape(remark),
                         )
                     },
                 }),
