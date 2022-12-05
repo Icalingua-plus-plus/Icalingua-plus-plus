9b9194cfaff0d4169d009d6870ede1be775a8070
ttimochan <teemo@timochan.cn>
2022-12-05 13:00:18 +0800

    fix : start script [skip ci]

--- icalingua-bridge-oicq/run.sh
+++ icalingua-bridge-oicq/run.sh
@@ -2,9 +2,9 @@
  # @Author: ttimochan
  # @Date: 2022-12-05 11:11:31
  # @LastEditors: ttimochan
- # @LastEditTime: 2022-12-05 11:12:49
+ # @LastEditTime: 2022-12-05 12:59:56
  # @FilePath: /Icalingua-plus-plus/icalingua-bridge-oicq/run.sh
 ### 
-#!/bin/bash
+#!/bin/sh
 cd /app/Icalingua-plus-plus/icalingua-bridge-oicq/
 node build
