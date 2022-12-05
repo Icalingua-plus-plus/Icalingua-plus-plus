367594c3c03fcf294f669374dd15942c58b91e90
ttimochan <teemo@timochan.cn>
2022-12-05 11:17:11 +0800

    fix: test

---
+++ icalingua-bridge-oicq/run.sh
@@ -0,0 +1,10 @@
+###
+ # @Author: ttimochan
+ # @Date: 2022-12-05 11:11:31
+ # @LastEditors: ttimochan
+ # @LastEditTime: 2022-12-05 11:12:49
+ # @FilePath: /Icalingua-plus-plus/icalingua-bridge-oicq/run.sh
+### 
+#!/bin/bash
+cd /app/Icalingua-plus-plus/icalingua-bridge-oicq/
+node build
