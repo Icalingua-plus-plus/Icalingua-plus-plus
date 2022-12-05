f4ad19f577fe7708f135ccf13cdb26e020c13fe0
ttimochan <teemo@timochan.cn>
2022-12-05 13:06:19 +0800

    deploy test

--- icalingua-bridge-oicq/run.sh
+++ icalingua-bridge-oicq/run.sh
@@ -2,9 +2,10 @@
  # @Author: ttimochan
  # @Date: 2022-12-05 11:11:31
  # @LastEditors: ttimochan
- # @LastEditTime: 2022-12-05 12:59:56
+ # @LastEditTime: 2022-12-05 13:06:16
  # @FilePath: /Icalingua-plus-plus/icalingua-bridge-oicq/run.sh
 ### 
 #!/bin/sh
 cd /app/Icalingua-plus-plus/icalingua-bridge-oicq/
+
 node build
