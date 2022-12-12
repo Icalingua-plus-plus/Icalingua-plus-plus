2a42f4b9c029f8a92e9a1bdc7fe99cd490b6a44e
Icalinguaplusplus <Icalinguaplusplus@protonmail.com>
2022-12-12 22:07:29 +0800

    fix: copy file download link

--- icalingua/static/fileManagerPreload.js
+++ icalingua/static/fileManagerPreload.js
@@ -1,2 +1,2 @@
 window.download=(url, out)=>require('electron').ipcRenderer.send('download', url, out)
-window.copyText=(text)=>require('electron').clipboard.writeText(text)
+window.copyText=(text)=>navigator.clipboard.writeText(text)
