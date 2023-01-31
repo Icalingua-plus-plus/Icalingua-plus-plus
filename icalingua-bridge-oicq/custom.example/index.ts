a3f6df61e50bfa200b201eada6b94c291dbbd473
Icalinguaplusplus <Icalinguaplusplus@protonmail.com>
2023-01-31 16:47:49 +0800

    docs: update custom plugin

--- icalingua-bridge-oicq/custom.example/index.ts
+++ icalingua-bridge-oicq/custom.example/index.ts
@@ -1,9 +1,7 @@
-export const onMessage = (data, bot) => {
+export const onMessage = (data: import('oicq-icalingua-plus-plus').MessageEventData, bot) => {
     // 只在收到消息时会触发
     // 此处 data 为 OICQ v1 的消息格式，bot 为 OICQ v1 的 Client ，可使用 OICQ v1 的 API
     console.log(data)
-    if (!data.message[0]) return
-    if ((data.message[0] && data.message[0].type !== 'text') || data.message[0].data.text !== '/help') return
-    if (data.message_type === 'private') bot.sendPrivateMsg(data.sender.user_id, '你好，我是 Icalingua++ Bot！', true)
-    else bot.sendGroupMsg(data.group_id, '你好，我是 Icalingua++ Bot！', true)
+    if (!data.raw_message) return
+    if (data.raw_message === '/help') data.reply('你好，我是 Icalingua++ Bot！')
 }
