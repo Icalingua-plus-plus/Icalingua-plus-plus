db29327e290d94dc93666e3504f703ebb80da007
Icalinguaplusplus <Icalinguaplusplus@protonmail.com>
2023-02-04 23:49:17 +0800

    feat: quick download image in image viewer

--- icalingua/static/imgViewPreload.js
+++ icalingua/static/imgViewPreload.js
@@ -1,2 +1,3 @@
 window.copyImage = (url) => require('electron').ipcRenderer.send('copyImage', url)
 window.saveSticker = (url) => require('electron').ipcRenderer.send('saveSticker', url)
+window.downloadImage = (url) => require('electron').ipcRenderer.send('downloadImage', url)
