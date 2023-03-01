818b878fa653855ac68c0a03a796d8a198472fb9
Icalinguaplusplus <Icalinguaplusplus@protonmail.com>
2023-03-01 20:44:26 +0800

    release: v2.9.0

--- icalingua-bridge-oicq/providers/socketIoProvider.ts
+++ icalingua-bridge-oicq/providers/socketIoProvider.ts
@@ -31,7 +31,7 @@ export const init = (adapter: typeof oicqAdapter) => {
         console.log('new client connected')
         //客户端对这个服务器发来的时间用私钥签名给服务端验证
         const salt = md5(new Date().getTime().toString())
-        socket.onAny(console.log)
+        //socket.onAny(console.log)
         socket.emit('requireAuth', salt, {
             version,
             protocolVersion,
