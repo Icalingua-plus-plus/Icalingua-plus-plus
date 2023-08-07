59d905fb5bc8c6a0222de9878d6e23c260f534d6
Icalinguaplusplus <Icalinguaplusplus@protonmail.com>
2023-08-07 11:46:38 +0800

    fix: update config

--- icalingua/src/main/utils/configManager.ts
+++ icalingua/src/main/utils/configManager.ts
@@ -111,6 +111,22 @@ if (fs.existsSync(configFilePath)) {
             config[i] = defaultConfig[i]
         }
     }
+    for (const i in emptyLoginForm) {
+        if (!(i in config['account'])) {
+            config['account'][i] = emptyLoginForm[i]
+        }
+    }
+    for (const i in defaultAria2Config) {
+        if (!(i in config['aria2'])) {
+            config['aria2'][i] = defaultAria2Config[i]
+        }
+    }
+    for (const i in defaultWinSize) {
+        if (!(i in config['winSize'])) {
+            config['winSize'][i] = defaultWinSize[i]
+        }
+    }
+
     if (typeof config.darkTaskIcon === 'boolean') config.darkTaskIcon = config.darkTaskIcon ? 'true' : 'false'
     saveConfigFile()
 } else {
