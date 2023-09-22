7d013f7262b288e539a1497f5ec8897e1333aec2
Icalinguaplusplus <Icalinguaplusplus@protonmail.com>
2023-09-22 15:23:33 +0800

    refactor: 语音转换

--- icalingua-bridge-oicq/utils/silkDecode.ts
+++ icalingua-bridge-oicq/utils/silkDecode.ts
@@ -9,34 +9,39 @@ export default async (url: string) => {
     const md5 = require('crypto').createHash('md5').update(res.data).digest('hex')
     const Path = require('path').join(require.main ? require.main.path : process.cwd(), 'data', 'records')
     const filePath = require('path').join(Path, md5 + '.ogg')
-    if (fs.existsSync(filePath)) {
-        return md5 + '.ogg'
-    }
-    const bufOgg = await conventSilk(res.data)
-
+    const rawFilePath = require('path').join(Path, md5)
     if (!fs.existsSync(Path)) {
         fs.mkdirSync(Path)
     }
-    fs.writeFileSync(filePath, bufOgg)
+    if (fs.existsSync(filePath)) {
+        return md5 + '.ogg'
+    }
+    if (!fs.existsSync(rawFilePath)) {
+        fs.writeFileSync(rawFilePath, res.data)
+    }
+    await conventSilk(rawFilePath, filePath)
     return md5 + '.ogg'
 }
 
-const conventSilk = (silkBuf: Buffer): Promise<Buffer> => {
+const conventSilk = (rawFilePath: string, filePath: string): Promise<void> => {
     return new Promise((resolve, reject) => {
         const child = fork(require('path').join(__dirname, '../static/silkchild.js'))
-        child.on('message', (bufOggStr: String) => {
-            const bufOgg = Buffer.from(bufOggStr, 'binary')
-            console.log('bufOgg received from child length:', bufOgg.length)
-            resolve(bufOgg)
+        child.on('message', (msg: string) => {
+            console.log('[silkDecode] Child success:', msg)
+            resolve()
         })
         child.on('error', (err) => {
             console.error(err)
             reject(err)
         })
         child.on('exit', (code, signal) => {
-            console.log(`Child process exited with code ${code} and signal ${signal}`)
-            if (code !== 0) reject(new Error(`Child process exited with code ${code} and signal ${signal}`))
+            console.log(`[silkDecode] Child process exited with code ${code} and signal ${signal}`)
+            if (code !== 0)
+                reject(new Error(`[silkDecode] Child process exited with code ${code} and signal ${signal}`))
         })
-        child.send(silkBuf.toString('binary'))
+        child.send({
+            rawFilePath,
+            filePath,
+        })
     })
 }
--- icalingua/static/silkchild.js
+++ icalingua/static/silkchild.js
@@ -1,69 +1,38 @@
 const silk = require('silk-sdk')
 const ffmpeg = require('fluent-ffmpeg')
-const PassThrough = require('stream').PassThrough
+const fs = require('fs')
 
-process.on('message', async (silkBufStr) => {
-    const silkBuf = Buffer.from(silkBufStr, 'binary');
-    let bufPcm, bufOgg, silkDecodeFailed = false
+process.on('message', async (pathConfig) => {
+    let silkDecodeFailed = false
     try {
-        bufPcm = silk.decode(silkBuf)
+        const silkBuf = fs.readFileSync(pathConfig.rawFilePath)
+        const bufPcm = silk.decode(silkBuf)
+        pathConfig.rawFilePath += '.pcm'
+        fs.writeFileSync(pathConfig.rawFilePath, bufPcm)
     } catch (err) {
         // 可能是 amr 語音，嘗試直接轉換
         console.error(err)
         silkDecodeFailed = true
     } finally {
-        bufOgg = await convertToOgg(silkDecodeFailed ? silkBuf : bufPcm, !silkDecodeFailed)
-        process.send(bufOgg.toString('binary'))
+        const msg = await convertToOgg(pathConfig, !silkDecodeFailed)
+        process.send(msg)
         process.exit(0)
     }
 })
 
-console.log('[Child process] Child process start! Start to convert record to ogg!');
+console.log('[silkDecode][Child] Child process start! Start to convert record to ogg!');
 
-const convertToOgg = (pcm, isPcm = true) => {
+const convertToOgg = (pathConfig, isPcm = true) => {
     return new Promise((resolve, reject) => {
-        const inStream = new PassThrough()
-        const outStream = new PassThrough()
-        inStream.end(pcm)
-        ffmpeg(inStream, { timeout: 10 })
+        ffmpeg(pathConfig.rawFilePath, { timeout: 60 })
             .inputOption(isPcm ? ['-f', 's16le', '-ar', '24000', '-ac', '1'] : [])
             .outputFormat('ogg')
             .on('error', (err) => {
-                // Stream 超时，写出 pcm 文件以转换
-                const filename = require('path').join(require('os').tmpdir(), Date.now() + '')
-                require('fs').writeFileSync(filename, pcm)
                 console.error(err)
-                ffmpeg(filename, { timeout: 15 })
-                .inputOption(isPcm ? ['-f', 's16le', '-ar', '24000', '-ac', '1'] : [])
-                .outputFormat('ogg')
-                .on('error', (err) => {
-                    console.error(err)
-                    reject(err)
-                })
-                .on('end', async () => {
-                    try {
-                        const buf = require('fs').readFileSync(filename + '.ogg')
-                        resolve(buf)
-                        require('fs').unlinkSync(filename)
-                        require('fs').unlinkSync(filename + '.ogg')
-                    } catch (err) {
-                        reject(err)
-                    }
-                }).save(filename + '.ogg')
+                reject(err)
             })
             .on('end', async () => {
-                const buf = await streamToBuffer(outStream)
-                resolve(buf)
-            })
-            .pipe(outStream, { end: true })
-    })
-}
-
-function streamToBuffer(stream) {
-    return new Promise((resolve, reject) => {
-        let buffers = []
-        stream.on('error', reject)
-        stream.on('data', (data) => buffers.push(data))
-        stream.on('end', () => resolve(Buffer.concat(buffers)))
+                resolve('ffmpeg convert success!')
+            }).save(pathConfig.filePath)
     })
 }
