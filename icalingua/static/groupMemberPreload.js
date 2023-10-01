fc82e5026aa75d183dfcdb46a8ead968ef12fac8
Icalinguaplusplus <Icalinguaplusplus@protonmail.com>
2023-10-02 00:00:35 +0800

    feat: 适配修改后的Q群管理

--- icalingua/static/groupMemberPreload.js
+++ icalingua/static/groupMemberPreload.js
@@ -20,6 +20,18 @@ Object.defineProperty(XMLHttpRequest.prototype, "responseText", {
                 data.manage = data.manage || [];
                 data.manage = data.manage.filter((e) => e.gc === gc);
 
+                if (data.join[0]) {
+                    data.manage.push(data.join[0]);
+                    XMLHttpRequest.prototype._open = XMLHttpRequest.prototype.open;
+                    XMLHttpRequest.prototype.open = function(){
+                        console.log(arguments);
+                        if (arguments[1].includes('delete_group_member') || 
+                            arguments[1].includes('set_group_card'))
+                            throw new Error('你不是管理员，无法完成此操作');
+                        this._open.call(this,...arguments);
+                    }
+                }
+
                 response = JSON.stringify(data);
                 const groups = data.join.concat(data.create, data.manage);
                 const groupName = groups.length ? groups[0].gn : "";
