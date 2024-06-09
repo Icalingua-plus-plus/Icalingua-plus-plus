1610a18bb53c2a1486a8a1e7cc2c643476e0cbb8
Icalinguaplusplus <Icalinguaplusplus@protonmail.com>
2024-06-09 23:21:01 +0800

    perf: 加快lottie加载速度

--- icalingua/src/renderer/components/LottieAnimation.vue
+++ icalingua/src/renderer/components/LottieAnimation.vue
@@ -56,7 +56,7 @@ export default {
         rendererSettings: {
             scaleMode: 'centerCrop',
             clearCanvas: true,
-            progressiveLoad: false,
+            progressiveLoad: true,
             hideOnTransparent: true,
         },
         anim: null,
