bac705d2b19fd5299439c807343aa2ca9a13dafd
Icalinguaplusplus <Icalinguaplusplus@protonmail.com>
2024-06-11 23:58:04 +0800

    fix: 超级表情炸了

--- icalingua/src/renderer/utils/getLottieFace.ts
+++ icalingua/src/renderer/utils/getLottieFace.ts
@@ -10,10 +10,10 @@ export default (msgText: string, time: number, result?: boolean): string | undef
     const idMatch = msgText.match(/^\[QLottie: (\d+)\,(\d+)\]$/) || msgText.match(/^\[QLottie: (\d+)\,(\d+)\,(\d+)\]$/)
     if (idMatch) {
         const lottie = faceIdToLottie.get(parseInt(idMatch[2]))
-        if (lottie.lottieType === 3 && idMatch[3]) {
-            result = true
-        }
         if (lottie) {
+            if (lottie.lottieType === 3 && idMatch[3]) {
+                result = true
+            }
             return getLottiePath(lottie.lottieId, result ? idMatch[3] : undefined, lottie.packId)
         } else {
             return getLottiePath(idMatch[2], result ? idMatch[3] : undefined, '0')
