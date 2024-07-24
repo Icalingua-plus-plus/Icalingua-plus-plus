0316a5cf879cb14c4511755ed46a3f7f430513aa
Icalinguaplusplus <Icalinguaplusplus@protonmail.com>
2024-07-24 10:41:20 +0800

    fix: oldVal undefined

--- icalingua/src/renderer/components/vac-mod/ChatWindow/Room/Room.vue
+++ icalingua/src/renderer/components/vac-mod/ChatWindow/Room/Room.vue
@@ -678,7 +678,7 @@ export default {
 
             if (this.infiniteState.head) {
                 this.infiniteState.head.loaded()
-            } else if (newVal.length && !this.scrollIcon && newVal.length !== oldVal.length) {
+            } else if (newVal.length && !this.scrollIcon && !(newVal && oldVal && newVal.length === oldVal.length)) {
                 setTimeout(() => {
                     element.scrollTo({ top: element.scrollHeight })
                     this.loadingMessages = false
