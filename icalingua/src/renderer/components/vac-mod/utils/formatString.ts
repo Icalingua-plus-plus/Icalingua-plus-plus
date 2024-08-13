510be6024e8949f43753cfb5861ef84cd599fce1
Icalinguaplusplus <Icalinguaplusplus@protonmail.com>
2024-08-14 00:37:32 +0800

    feat: 链接高亮增加排除符号

--- icalingua/src/renderer/components/vac-mod/utils/formatString.ts
+++ icalingua/src/renderer/components/vac-mod/utils/formatString.ts
@@ -268,6 +268,9 @@ const noLinkifyRegexs = [
     /<IcalinguaAt qq=\d+>[^<]+<\/IcalinguaAt>/g, //at 不要当作链接一部分
     /[^\x00-\xff]+/g, //中文和全角符号不要当作链接一部分
     /"/g, //引号不要当作链接一部分
+    /\(/g,
+    /\)/g, //括号不要当作链接一部分
+    /'/g, //单引号不要当作链接一部分
 ]
 const replacements = [] //存储替换数据的起始位置、结束位置、替换内容
 function replaceNoLinkifyCharacters(str) {
