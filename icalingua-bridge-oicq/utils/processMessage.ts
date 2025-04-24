ba8fb2b25658fbc256aa4897a086c1e5634299a9
Clansty <i@gao4.pw>
2025-04-24 16:20:27 +0800

    fix: at

--- icalingua-bridge-oicq/utils/processMessage.ts
+++ icalingua-bridge-oicq/utils/processMessage.ts
@@ -29,8 +29,10 @@ const createProcessMessage = (adapter: typeof oicqAdapter) => {
                         lastReply = true
                         break
                     }
-                    if (!m.data.text) {
-                        const info = await adapter._getGroupMemberInfo(-roomId, message.senderId, false)
+                    if (!m.data.text && (m.data.qq == 0 || m.data.qq == 'all')) {
+                        m.data.text = '@全体成员'
+                    } else if (!m.data.text) {
+                        const info = await adapter._getGroupMemberInfo(-roomId, m.data.qq as any, false)
                         m.data.text = '@' + (info.card || info.nickname)
                     }
                 // noinspection FallThroughInSwitchStatementJS 确信
