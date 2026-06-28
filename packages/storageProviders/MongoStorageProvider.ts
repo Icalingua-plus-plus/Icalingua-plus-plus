d972ccc2855e71f82839a0cbd9c569b1253a928e
Dimole <dmlgzs@qq.com>
2026-06-28 19:47:46 +0800

    Revert "chore(deps): 升级mongodb到v6版本"
    This reverts commit 9f540e3b70175aaee187b1f1243f6caffc234baf.

--- packages/storageProviders/MongoStorageProvider.ts
+++ packages/storageProviders/MongoStorageProvider.ts
@@ -9,7 +9,6 @@ export default class MongoStorageProvider implements StorageProvider {
     id: string | number
     connStr: string
     mdb: Db
-    private client: MongoClient
 
     constructor(connStr: string, id: string | number) {
         this.id = id
@@ -43,10 +42,8 @@ export default class MongoStorageProvider implements StorageProvider {
     }
 
     async connect(): Promise<void> {
-        console.log('Connecting to MongoDB... Addr: ' + this.connStr)
-        this.client = new MongoClient(this.connStr)
-        await this.client.connect()
-        this.mdb = this.client.db('eqq' + this.id)
+        const dba = await MongoClient.connect(this.connStr)
+        this.mdb = dba.db('eqq' + this.id)
         await this.mdb.collection('rooms').createIndex('roomId', {
             background: true,
             unique: true,
@@ -96,7 +93,7 @@ export default class MongoStorageProvider implements StorageProvider {
 
     async updateMessage(roomId: number, messageId: string | number, message: Partial<Message>): Promise<any> {
         try {
-            return await this.mdb.collection('msg' + roomId).updateOne({ _id: messageId } as any, { $set: message })
+            return await this.mdb.collection('msg' + roomId).updateOne({ _id: messageId }, { $set: message })
         } catch (e) {}
     }
 
@@ -268,7 +265,7 @@ export default class MongoStorageProvider implements StorageProvider {
     }
 
     getUnreadCount(priority: number): Promise<number> {
-        return this.mdb.collection('rooms').countDocuments({
+        const unreadRooms = this.mdb.collection('rooms').find({
             unreadCount: {
                 $gt: 0,
             },
@@ -276,6 +273,7 @@ export default class MongoStorageProvider implements StorageProvider {
                 $gte: priority,
             },
         })
+        return unreadRooms.count()
     }
 
     getFirstUnreadRoom(priority: number): Promise<Room> {
